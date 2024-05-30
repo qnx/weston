385334b77a6a32d3428d19080535cf13df237a4f
Pekka Paalanen <pekka.paalanen@collabora.com>
2024-05-30 15:24:21 +0300

    color-noop: improve profile error messages
    While no-op does not support these, other color managers might. Change
    the wording so that the user does not think that Weston does not support
    these at all.
    
    Signed-off-by: Pekka Paalanen <pekka.paalanen@collabora.com>

--- libweston/color-noop.c
+++ libweston/color-noop.c
@@ -131,7 +131,7 @@ cmnoop_get_color_profile_from_icc(struct weston_color_manager *cm,
 				  struct weston_color_profile **cprof_out,
 				  char **errmsg)
 {
-	*errmsg = xstrdup("ICC profiles are unsupported.");
+	*errmsg = xstrdup("Error: color manager no-op does not support ICC profiles.");
 	return false;
 }
 
@@ -142,7 +142,7 @@ cmnoop_get_color_profile_from_params(struct weston_color_manager *cm,
 				     struct weston_color_profile **cprof_out,
 				     char **errmsg)
 {
-	*errmsg = xstrdup("parametric profiles are unsupported.");
+	*errmsg = xstrdup("Error: color manager no-op does not support parametric color profiles.");
 	return false;
 }
 
