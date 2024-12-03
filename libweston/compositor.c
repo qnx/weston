qnx/weston

commit 827e22761be81948aeeebe5024940414efa8618d

gl-renderer: Draw holes on primary plane for the view on underlay
Signed-off-by: Chao Guo <chao.guo@nxp.com>

libweston/compositor.c
@@ -225,6 +225,15 @@ maybe_replace_paint_node(struct weston_paint_node *pnode)
 		pnode->solid = placeholder_color;
 		return;
 	}
+	if (pnode->need_hole) {
+		pnode->draw_solid = true;
+		pnode->is_fully_opaque = true;
+		pnode->is_fully_blended = false;
+		pnode->solid = (struct weston_solid_buffer_values) {
+			               0.0, 0.0, 0.0, 0.0
+		               };
+		return;
+	}
 	if (surface->protection_mode !=
 	    WESTON_SURFACE_PROTECTION_MODE_ENFORCED)
 		return;

libweston/renderer-gl/gl-renderer.c
@@ -1665,7 +1665,8 @@ repaint_views(struct weston_output *output, pixman_region32_t *damage)
 
 	wl_list_for_each_reverse(pnode, &output->paint_node_z_order_list,
 				 z_order_link) {
-		if (pnode->plane == &output->primary_plane)
+		if (pnode->plane == &output->primary_plane ||
+		    pnode->need_hole)
 			draw_paint_node(pnode, damage);
 	}
 

commit 53189ebb89df65bee66088202a863cc5caec5af3

compositor: re-order paint node placeholder checks
Doing these in the wrong order breaks content protection, and breaks
placing direct-display paint nodes on underlays.

Fixes: 827e2276 ("gl-renderer: Draw holes on primary plane for the view on underlay")
Signed-off-by: Derek Foreman <derek.foreman@collabora.com>

libweston/compositor.c
@@ -217,14 +217,22 @@ maybe_replace_paint_node(struct weston_paint_node *pnode)
 
 	pnode->draw_solid = false;
 	pnode->is_direct = false;
-	if (buffer->direct_display) {
+	/* Check for content protection first, as we should always prevent
+	 * the rendering of protected content.
+	 */
+	if (surface->protection_mode ==
+	    WESTON_SURFACE_PROTECTION_MODE_ENFORCED &&
+	    (recording_censor || unprotected_censor)) {
 		pnode->draw_solid = true;
-		pnode->is_direct = true;
 		pnode->is_fully_opaque = true;
 		pnode->is_fully_blended = false;
 		pnode->solid = placeholder_color;
 		return;
 	}
+	/* Check if we need a hole before we check direct-display, otherwise
+	 * we'll end up drawing an opaque placeholder over direct_display
+	 * paint nodes when we place them on underlays.
+	 */
 	if (pnode->need_hole) {
 		pnode->draw_solid = true;
 		pnode->is_fully_opaque = true;
@@ -234,15 +242,13 @@ maybe_replace_paint_node(struct weston_paint_node *pnode)
 		               };
 		return;
 	}
-	if (surface->protection_mode !=
-	    WESTON_SURFACE_PROTECTION_MODE_ENFORCED)
-		return;
-
-	if (recording_censor || unprotected_censor) {
+	if (buffer->direct_display) {
 		pnode->draw_solid = true;
+		pnode->is_direct = true;
 		pnode->is_fully_opaque = true;
 		pnode->is_fully_blended = false;
 		pnode->solid = placeholder_color;
+		return;
 	}
 }
 