264c205add270010cffc549d3073658d186d1151
Derek Foreman <derek.foreman@collabora.com>
2024-12-11 10:03:37 -0600

    desktop-shell: Make backgrounds fully opaque
    Non-opaque backgrounds don't make sense, and cause rendering
    problems.
    
    Silently set backgrounds fully opaque, and remove any mention of
    alpha from the man page.
    
    Kiosk-shell already implicitly forces opaque backgrounds.
    
    Old weston.ini with FF for alpha will always continue to work.
    
    Signed-off-by: Derek Foreman <derek.foreman@collabora.com>

--- clients/desktop-shell.c
+++ clients/desktop-shell.c
@@ -1191,6 +1191,9 @@ background_create(struct desktop *desktop, struct output *output)
 	weston_config_section_get_color(s, "background-color",
 					&background->color, 0x00000000);
 
+	/* Backgrounds must be fully opaque. */
+	background->color |= 0xFF000000;
+
 	weston_config_section_get_string(s, "background-type",
 					 &type, "tile");
 	if (type == NULL) {
