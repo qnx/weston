1f169c044c8e7af04ee3e7647c834d6d056977d3
Loïc Molinari <loic.molinari@collabora.com>
2025-01-26 12:53:08 +0100

    tests: Port timespec test to Weston testing framework
    Signed-off-by: Loïc Molinari <loic.molinari@collabora.com>

--- tests/timespec-test.c
+++ tests/timespec-test.c
@@ -25,21 +25,11 @@
 
 #include "config.h"
 
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-#include <assert.h>
-#include <errno.h>
-#include <unistd.h>
-#include <time.h>
-
 #include "shared/timespec-util.h"
+#include "weston-test-client-helper.h"
+#include "weston-test-assert.h"
 
-#include "shared/helpers.h"
-#include "zunitc/zunitc.h"
-
-ZUC_TEST(timespec_test, timespec_sub)
+TEST(test_timespec_sub)
 {
 	struct timespec a, b, r;
 
@@ -48,38 +38,38 @@ ZUC_TEST(timespec_test, timespec_sub)
 	b.tv_sec = 0;
 	b.tv_nsec = 2;
 	timespec_sub(&r, &a, &b);
-	ZUC_ASSERT_EQ(r.tv_sec, 0);
-	ZUC_ASSERT_EQ(r.tv_nsec, NSEC_PER_SEC - 1);
+	test_assert_u64_eq(r.tv_sec, 0);
+	test_assert_u64_eq(r.tv_nsec, NSEC_PER_SEC - 1);
 }
 
-ZUC_TEST(timespec_test, timespec_to_nsec)
+TEST(test_timespec_to_nsec)
 {
 	struct timespec a;
 
 	a.tv_sec = 4;
 	a.tv_nsec = 4;
-	ZUC_ASSERT_EQ(timespec_to_nsec(&a), (NSEC_PER_SEC * 4ULL) + 4);
+	test_assert_u64_eq(timespec_to_nsec(&a), (NSEC_PER_SEC * 4ULL) + 4);
 }
 
-ZUC_TEST(timespec_test, timespec_to_usec)
+TEST(test_timespec_to_usec)
 {
 	struct timespec a;
 
 	a.tv_sec = 4;
 	a.tv_nsec = 4000;
-	ZUC_ASSERT_EQ(timespec_to_usec(&a), (4000000ULL) + 4);
+	test_assert_u64_eq(timespec_to_usec(&a), (4000000ULL) + 4);
 }
 
-ZUC_TEST(timespec_test, timespec_to_msec)
+TEST(test_timespec_to_msec)
 {
 	struct timespec a;
 
 	a.tv_sec = 4;
 	a.tv_nsec = 4000000;
-	ZUC_ASSERT_EQ(timespec_to_msec(&a), (4000ULL) + 4);
+	test_assert_u64_eq(timespec_to_msec(&a), (4000ULL) + 4);
 }
 
-ZUC_TEST(timespec_test, timespec_to_proto)
+TEST(test_timespec_to_proto)
 {
 	struct timespec a;
 	uint32_t tv_sec_hi;
@@ -89,100 +79,100 @@ ZUC_TEST(timespec_test, timespec_to_proto)
 	a.tv_sec = 0;
 	a.tv_nsec = 0;
 	timespec_to_proto(&a, &tv_sec_hi, &tv_sec_lo, &tv_nsec);
-	ZUC_ASSERT_EQ(0, tv_sec_hi);
-	ZUC_ASSERT_EQ(0, tv_sec_lo);
-	ZUC_ASSERT_EQ(0, tv_nsec);
+	test_assert_u64_eq(0, tv_sec_hi);
+	test_assert_u64_eq(0, tv_sec_lo);
+	test_assert_u64_eq(0, tv_nsec);
 
 	a.tv_sec = 1234;
 	a.tv_nsec = NSEC_PER_SEC - 1;
 	timespec_to_proto(&a, &tv_sec_hi, &tv_sec_lo, &tv_nsec);
-	ZUC_ASSERT_EQ(0, tv_sec_hi);
-	ZUC_ASSERT_EQ(1234, tv_sec_lo);
-	ZUC_ASSERT_EQ(NSEC_PER_SEC - 1, tv_nsec);
+	test_assert_u64_eq(0, tv_sec_hi);
+	test_assert_u64_eq(1234, tv_sec_lo);
+	test_assert_u64_eq(NSEC_PER_SEC - 1, tv_nsec);
 
 	a.tv_sec = (time_t)0x7000123470005678LL;
 	a.tv_nsec = 1;
 	timespec_to_proto(&a, &tv_sec_hi, &tv_sec_lo, &tv_nsec);
-	ZUC_ASSERT_EQ((uint64_t)a.tv_sec >> 32, tv_sec_hi);
-	ZUC_ASSERT_EQ(0x70005678, tv_sec_lo);
-	ZUC_ASSERT_EQ(1, tv_nsec);
+	test_assert_u64_eq((uint64_t)a.tv_sec >> 32, tv_sec_hi);
+	test_assert_u64_eq(0x70005678, tv_sec_lo);
+	test_assert_u64_eq(1, tv_nsec);
 }
 
-ZUC_TEST(timespec_test, millihz_to_nsec)
+TEST(test_millihz_to_nsec)
 {
-	ZUC_ASSERT_EQ(millihz_to_nsec(60000), 16666666);
+	test_assert_u64_eq(millihz_to_nsec(60000), 16666666);
 }
 
-ZUC_TEST(timespec_test, timespec_add_nsec)
+TEST(test_timespec_add_nsec)
 {
 	struct timespec a, r;
 
 	a.tv_sec = 0;
 	a.tv_nsec = NSEC_PER_SEC - 1;
 	timespec_add_nsec(&r, &a, 1);
-	ZUC_ASSERT_EQ(1, r.tv_sec);
-	ZUC_ASSERT_EQ(0, r.tv_nsec);
+	test_assert_u64_eq(1, r.tv_sec);
+	test_assert_u64_eq(0, r.tv_nsec);
 
 	timespec_add_nsec(&r, &a, 2);
-	ZUC_ASSERT_EQ(1, r.tv_sec);
-	ZUC_ASSERT_EQ(1, r.tv_nsec);
+	test_assert_u64_eq(1, r.tv_sec);
+	test_assert_u64_eq(1, r.tv_nsec);
 
 	timespec_add_nsec(&r, &a, (NSEC_PER_SEC * 2ULL));
-	ZUC_ASSERT_EQ(2, r.tv_sec);
-	ZUC_ASSERT_EQ(NSEC_PER_SEC - 1, r.tv_nsec);
+	test_assert_u64_eq(2, r.tv_sec);
+	test_assert_u64_eq(NSEC_PER_SEC - 1, r.tv_nsec);
 
 	timespec_add_nsec(&r, &a, (NSEC_PER_SEC * 2ULL) + 2);
-	ZUC_ASSERT_EQ(r.tv_sec, 3);
-	ZUC_ASSERT_EQ(r.tv_nsec, 1);
+	test_assert_u64_eq(r.tv_sec, 3);
+	test_assert_u64_eq(r.tv_nsec, 1);
 
 	a.tv_sec = 1;
 	a.tv_nsec = 1;
 	timespec_add_nsec(&r, &a, -2);
-	ZUC_ASSERT_EQ(r.tv_sec, 0);
-	ZUC_ASSERT_EQ(r.tv_nsec, NSEC_PER_SEC - 1);
+	test_assert_u64_eq(r.tv_sec, 0);
+	test_assert_u64_eq(r.tv_nsec, NSEC_PER_SEC - 1);
 
 	a.tv_nsec = 0;
 	timespec_add_nsec(&r, &a, -NSEC_PER_SEC);
-	ZUC_ASSERT_EQ(0, r.tv_sec);
-	ZUC_ASSERT_EQ(0, r.tv_nsec);
+	test_assert_u64_eq(0, r.tv_sec);
+	test_assert_u64_eq(0, r.tv_nsec);
 
 	a.tv_nsec = 0;
 	timespec_add_nsec(&r, &a, -NSEC_PER_SEC + 1);
-	ZUC_ASSERT_EQ(0, r.tv_sec);
-	ZUC_ASSERT_EQ(1, r.tv_nsec);
+	test_assert_u64_eq(0, r.tv_sec);
+	test_assert_u64_eq(1, r.tv_nsec);
 
 	a.tv_nsec = 50;
 	timespec_add_nsec(&r, &a, (-NSEC_PER_SEC * 10ULL));
-	ZUC_ASSERT_EQ(-9, r.tv_sec);
-	ZUC_ASSERT_EQ(50, r.tv_nsec);
+	test_assert_u64_eq(-9, r.tv_sec);
+	test_assert_u64_eq(50, r.tv_nsec);
 
 	r.tv_sec = 4;
 	r.tv_nsec = 0;
 	timespec_add_nsec(&r, &r, NSEC_PER_SEC + 10ULL);
-	ZUC_ASSERT_EQ(5, r.tv_sec);
-	ZUC_ASSERT_EQ(10, r.tv_nsec);
+	test_assert_u64_eq(5, r.tv_sec);
+	test_assert_u64_eq(10, r.tv_nsec);
 
 	timespec_add_nsec(&r, &r, (NSEC_PER_SEC * 3ULL) - 9ULL);
-	ZUC_ASSERT_EQ(8, r.tv_sec);
-	ZUC_ASSERT_EQ(1, r.tv_nsec);
+	test_assert_u64_eq(8, r.tv_sec);
+	test_assert_u64_eq(1, r.tv_nsec);
 
 	timespec_add_nsec(&r, &r, (NSEC_PER_SEC * 7ULL) + (NSEC_PER_SEC - 1ULL));
-	ZUC_ASSERT_EQ(16, r.tv_sec);
-	ZUC_ASSERT_EQ(0, r.tv_nsec);
+	test_assert_u64_eq(16, r.tv_sec);
+	test_assert_u64_eq(0, r.tv_nsec);
 }
 
-ZUC_TEST(timespec_test, timespec_add_msec)
+TEST(test_timespec_add_msec)
 {
 	struct timespec a, r;
 
 	a.tv_sec = 1000;
 	a.tv_nsec = 1;
 	timespec_add_msec(&r, &a, 2002);
-	ZUC_ASSERT_EQ(1002, r.tv_sec);
-	ZUC_ASSERT_EQ(2000001, r.tv_nsec);
+	test_assert_u64_eq(1002, r.tv_sec);
+	test_assert_u64_eq(2000001, r.tv_nsec);
 }
 
-ZUC_TEST(timespec_test, timespec_sub_to_nsec)
+TEST(test_timespec_sub_to_nsec)
 {
 	struct timespec a, b;
 
@@ -190,10 +180,10 @@ ZUC_TEST(timespec_test, timespec_sub_to_nsec)
 	a.tv_nsec = 1;
 	b.tv_sec = 1;
 	b.tv_nsec = 2;
-	ZUC_ASSERT_EQ((999LL * NSEC_PER_SEC) - 1, timespec_sub_to_nsec(&a, &b));
+	test_assert_u64_eq((999LL * NSEC_PER_SEC) - 1, timespec_sub_to_nsec(&a, &b));
 }
 
-ZUC_TEST(timespec_test, timespec_sub_to_msec)
+TEST(test_timespec_sub_to_msec)
 {
 	struct timespec a, b;
 
@@ -201,108 +191,108 @@ ZUC_TEST(timespec_test, timespec_sub_to_msec)
 	a.tv_nsec = 2000000L;
 	b.tv_sec = 2;
 	b.tv_nsec = 1000000L;
-	ZUC_ASSERT_EQ((998 * 1000) + 1, timespec_sub_to_msec(&a, &b));
+	test_assert_u64_eq((998 * 1000) + 1, timespec_sub_to_msec(&a, &b));
 }
 
-ZUC_TEST(timespec_test, timespec_from_nsec)
+TEST(test_timespec_from_nsec)
 {
 	struct timespec a;
 
 	timespec_from_nsec(&a, 0);
-	ZUC_ASSERT_EQ(0, a.tv_sec);
-	ZUC_ASSERT_EQ(0, a.tv_nsec);
+	test_assert_u64_eq(0, a.tv_sec);
+	test_assert_u64_eq(0, a.tv_nsec);
 
 	timespec_from_nsec(&a, NSEC_PER_SEC - 1);
-	ZUC_ASSERT_EQ(0, a.tv_sec);
-	ZUC_ASSERT_EQ(NSEC_PER_SEC - 1, a.tv_nsec);
+	test_assert_u64_eq(0, a.tv_sec);
+	test_assert_u64_eq(NSEC_PER_SEC - 1, a.tv_nsec);
 
 	timespec_from_nsec(&a, NSEC_PER_SEC);
-	ZUC_ASSERT_EQ(1, a.tv_sec);
-	ZUC_ASSERT_EQ(0, a.tv_nsec);
+	test_assert_u64_eq(1, a.tv_sec);
+	test_assert_u64_eq(0, a.tv_nsec);
 
 	timespec_from_nsec(&a, (5LL * NSEC_PER_SEC) + 1);
-	ZUC_ASSERT_EQ(5, a.tv_sec);
-	ZUC_ASSERT_EQ(1, a.tv_nsec);
+	test_assert_u64_eq(5, a.tv_sec);
+	test_assert_u64_eq(1, a.tv_nsec);
 }
 
-ZUC_TEST(timespec_test, timespec_from_usec)
+TEST(test_timespec_from_usec)
 {
 	struct timespec a;
 
 	timespec_from_usec(&a, 0);
-	ZUC_ASSERT_EQ(0, a.tv_sec);
-	ZUC_ASSERT_EQ(0, a.tv_nsec);
+	test_assert_u64_eq(0, a.tv_sec);
+	test_assert_u64_eq(0, a.tv_nsec);
 
 	timespec_from_usec(&a, 999999);
-	ZUC_ASSERT_EQ(0, a.tv_sec);
-	ZUC_ASSERT_EQ(999999 * 1000, a.tv_nsec);
+	test_assert_u64_eq(0, a.tv_sec);
+	test_assert_u64_eq(999999 * 1000, a.tv_nsec);
 
 	timespec_from_usec(&a, 1000000);
-	ZUC_ASSERT_EQ(1, a.tv_sec);
-	ZUC_ASSERT_EQ(0, a.tv_nsec);
+	test_assert_u64_eq(1, a.tv_sec);
+	test_assert_u64_eq(0, a.tv_nsec);
 
 	timespec_from_usec(&a, 5000001);
-	ZUC_ASSERT_EQ(5, a.tv_sec);
-	ZUC_ASSERT_EQ(1000, a.tv_nsec);
+	test_assert_u64_eq(5, a.tv_sec);
+	test_assert_u64_eq(1000, a.tv_nsec);
 }
 
-ZUC_TEST(timespec_test, timespec_from_msec)
+TEST(test_timespec_from_msec)
 {
 	struct timespec a;
 
 	timespec_from_msec(&a, 0);
-	ZUC_ASSERT_EQ(0, a.tv_sec);
-	ZUC_ASSERT_EQ(0, a.tv_nsec);
+	test_assert_u64_eq(0, a.tv_sec);
+	test_assert_u64_eq(0, a.tv_nsec);
 
 	timespec_from_msec(&a, 999);
-	ZUC_ASSERT_EQ(0, a.tv_sec);
-	ZUC_ASSERT_EQ(999 * 1000000, a.tv_nsec);
+	test_assert_u64_eq(0, a.tv_sec);
+	test_assert_u64_eq(999 * 1000000, a.tv_nsec);
 
 	timespec_from_msec(&a, 1000);
-	ZUC_ASSERT_EQ(1, a.tv_sec);
-	ZUC_ASSERT_EQ(0, a.tv_nsec);
+	test_assert_u64_eq(1, a.tv_sec);
+	test_assert_u64_eq(0, a.tv_nsec);
 
 	timespec_from_msec(&a, 5001);
-	ZUC_ASSERT_EQ(5, a.tv_sec);
-	ZUC_ASSERT_EQ(1000000, a.tv_nsec);
+	test_assert_u64_eq(5, a.tv_sec);
+	test_assert_u64_eq(1000000, a.tv_nsec);
 }
 
-ZUC_TEST(timespec_test, timespec_from_proto)
+TEST(test_timespec_from_proto)
 {
 	struct timespec a;
 
 	timespec_from_proto(&a, 0, 0, 0);
-	ZUC_ASSERT_EQ(0, a.tv_sec);
-	ZUC_ASSERT_EQ(0, a.tv_nsec);
+	test_assert_u64_eq(0, a.tv_sec);
+	test_assert_u64_eq(0, a.tv_nsec);
 
 	timespec_from_proto(&a, 0, 1234, 9999);
-	ZUC_ASSERT_EQ(1234, a.tv_sec);
-	ZUC_ASSERT_EQ(9999, a.tv_nsec);
+	test_assert_u64_eq(1234, a.tv_sec);
+	test_assert_u64_eq(9999, a.tv_nsec);
 
 	timespec_from_proto(&a, 0x1234, 0x5678, 1);
-	ZUC_ASSERT_EQ((time_t)0x0000123400005678LL, a.tv_sec);
-	ZUC_ASSERT_EQ(1, a.tv_nsec);
+	test_assert_u64_eq((time_t)0x0000123400005678LL, a.tv_sec);
+	test_assert_u64_eq(1, a.tv_nsec);
 }
 
-ZUC_TEST(timespec_test, timespec_is_zero)
+TEST(test_timespec_is_zero)
 {
 	struct timespec zero = { 0 };
 	struct timespec non_zero_sec = { .tv_sec = 1, .tv_nsec = 0 };
 	struct timespec non_zero_nsec = { .tv_sec = 0, .tv_nsec = 1 };
 
-	ZUC_ASSERT_TRUE(timespec_is_zero(&zero));
-	ZUC_ASSERT_FALSE(timespec_is_zero(&non_zero_nsec));
-	ZUC_ASSERT_FALSE(timespec_is_zero(&non_zero_sec));
+	test_assert_true(timespec_is_zero(&zero));
+	test_assert_false(timespec_is_zero(&non_zero_nsec));
+	test_assert_false(timespec_is_zero(&non_zero_sec));
 }
 
-ZUC_TEST(timespec_test, timespec_eq)
+TEST(test_timespec_eq)
 {
 	struct timespec a = { .tv_sec = 2, .tv_nsec = 1 };
 	struct timespec b = { .tv_sec = -1, .tv_nsec = 2 };
 
-	ZUC_ASSERT_TRUE(timespec_eq(&a, &a));
-	ZUC_ASSERT_TRUE(timespec_eq(&b, &b));
+	test_assert_true(timespec_eq(&a, &a));
+	test_assert_true(timespec_eq(&b, &b));
 
-	ZUC_ASSERT_FALSE(timespec_eq(&a, &b));
-	ZUC_ASSERT_FALSE(timespec_eq(&b, &a));
+	test_assert_false(timespec_eq(&a, &b));
+	test_assert_false(timespec_eq(&b, &a));
 }
