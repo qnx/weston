qnx/weston

commit 3fefb5ba4468c7995abd3c4e878c526e386b224e

libweston, clients, tests: implement weston_matrix in terms of weston_mat4f
This converts weston_matrix and weston_vector to use linalg-types
internally. All direct accesses to members had to be converted
everywhere, mostly in the simplest form possible which leaves some
trivially reducable code around.

The intention is that we can now gradually migrate away from
weston_matrix in the future.

Look like one trailing space got accidentally annihilated in
compositor.c.

Signed-off-by: Pekka Paalanen <pekka.paalanen@collabora.com>

shared/matrix.c
@@ -48,66 +48,37 @@
 WL_EXPORT void
 weston_matrix_init(struct weston_matrix *matrix)
 {
-	static const struct weston_matrix identity = {
-		.d = { 1, 0, 0, 0,  0, 1, 0, 0,  0, 0, 1, 0,  0, 0, 0, 1 },
-		.type = 0,
-	};
-
-	memcpy(matrix, &identity, sizeof identity);
+	matrix->M = WESTON_MAT4F_IDENTITY;
+	matrix->type = 0;
 }
 
 /* m <- n * m, that is, m is multiplied on the LEFT. */
 WL_EXPORT void
 weston_matrix_multiply(struct weston_matrix *m, const struct weston_matrix *n)
 {
-	struct weston_matrix tmp;
-	const float *row, *column;
-	int i, j, k;
-
-	for (i = 0; i < 4; i++) {
-		row = m->d + i * 4;
-		for (j = 0; j < 4; j++) {
-			tmp.d[4 * i + j] = 0;
-			column = n->d + j;
-			for (k = 0; k < 4; k++)
-				tmp.d[4 * i + j] += row[k] * column[k * 4];
-		}
-	}
-	tmp.type = m->type | n->type;
-	memcpy(m, &tmp, sizeof tmp);
+	m->M = weston_m4f_mul_m4f(n->M, m->M);
+	m->type |= n->type;
 }
 
 WL_EXPORT void
 weston_matrix_translate(struct weston_matrix *matrix, float x, float y, float z)
 {
-	struct weston_matrix translate = {
-		.d = { 1, 0, 0, 0,  0, 1, 0, 0,  0, 0, 1, 0,  x, y, z, 1 },
-		.type = WESTON_MATRIX_TRANSFORM_TRANSLATE,
-	};
-
-	weston_matrix_multiply(matrix, &translate);
+	matrix->M = weston_m4f_mul_m4f(weston_m4f_translation(x, y, z), matrix->M);
+	matrix->type = WESTON_MATRIX_TRANSFORM_TRANSLATE;
 }
 
 WL_EXPORT void
 weston_matrix_scale(struct weston_matrix *matrix, float x, float y,float z)
 {
-	struct weston_matrix scale = {
-		.d = { x, 0, 0, 0,  0, y, 0, 0,  0, 0, z, 0,  0, 0, 0, 1 },
-		.type = WESTON_MATRIX_TRANSFORM_SCALE,
-	};
-
-	weston_matrix_multiply(matrix, &scale);
+	matrix->M = weston_m4f_mul_m4f(weston_m4f_scaling(x, y, z), matrix->M);
+	matrix->type = WESTON_MATRIX_TRANSFORM_SCALE;
 }
 
 WL_EXPORT void
 weston_matrix_rotate_xy(struct weston_matrix *matrix, float cos, float sin)
 {
-	struct weston_matrix translate = {
-		.d = { cos, sin, 0, 0,  -sin, cos, 0, 0,  0, 0, 1, 0,  0, 0, 0, 1 },
-		.type = WESTON_MATRIX_TRANSFORM_ROTATE,
-	};
-
-	weston_matrix_multiply(matrix, &translate);
+	matrix->M = weston_m4f_mul_m4f(weston_m4f_rotation_xy(cos, sin), matrix->M);
+	matrix->type = WESTON_MATRIX_TRANSFORM_ROTATE;
 }
 
 /* v <- m * v */
@@ -115,16 +86,7 @@ WL_EXPORT void
 weston_matrix_transform(const struct weston_matrix *matrix,
 			struct weston_vector *v)
 {
-	int i, j;
-	struct weston_vector t;
-
-	for (i = 0; i < 4; i++) {
-		t.f[i] = 0;
-		for (j = 0; j < 4; j++)
-			t.f[i] += v->f[j] * matrix->d[i + j * 4];
-	}
-
-	*v = t;
+	v->v = weston_m4f_mul_v4f(matrix->M, v->v);
 }
 
 WL_EXPORT struct weston_coord
@@ -132,15 +94,27 @@ weston_matrix_transform_coord(const struct weston_matrix *matrix,
 			      struct weston_coord c)
 {
 	struct weston_coord out;
-	struct weston_vector t = { { c.x, c.y, 0.0, 1.0 } };
+	struct weston_vector t = { .v.el = { c.x, c.y, 0.0, 1.0 } };
 
 	weston_matrix_transform(matrix, &t);
 
-	assert(fabsf(t.f[3]) > 1e-6);
+	assert(fabsf(t.v.el[3]) > 1e-6);
 
-	out.x = t.f[0] / t.f[3];
-	out.y = t.f[1] / t.f[3];
+	out.x = t.v.el[0] / t.v.el[3];
+	out.y = t.v.el[1] / t.v.el[3];
 	return out;
+}
+
+WL_EXPORT int
+weston_matrix_invert(struct weston_matrix *inverse,
+		     const struct weston_matrix *matrix)
+{
+	if (weston_m4f_invert(&inverse->M, matrix->M)) {
+		inverse->type = matrix->type;
+		return 0;
+	}
+
+	return -1;
 }
 
 static inline void
@@ -175,118 +149,6 @@ find_pivot(double *column, unsigned k)
 			p = k;
 
 	return p;
-}
-
-/*
- * reference: Gene H. Golub and Charles F. van Loan. Matrix computations.
- * 3rd ed. The Johns Hopkins University Press. 1996.
- * LU decomposition, forward and back substitution: Chapter 3.
- */
-
-static int
-matrix_invert(double *A, unsigned *p, const struct weston_matrix *matrix)
-{
-	unsigned i, j, k;
-	unsigned pivot;
-	double pv;
-
-	for (i = 0; i < 4; ++i)
-		p[i] = i;
-	for (i = 16; i--; )
-		A[i] = matrix->d[i];
-
-	/* LU decomposition with partial pivoting */
-	for (k = 0; k < 4; ++k) {
-		pivot = find_pivot(&A[k * 4], k);
-		if (pivot != k) {
-			swap_unsigned(&p[k], &p[pivot]);
-			swap_rows(&A[k], &A[pivot]);
-		}
-
-		pv = A[k * 4 + k];
-		if (fabs(pv) < 1e-9)
-			return -1; /* zero pivot, not invertible */
-
-		for (i = k + 1; i < 4; ++i) {
-			A[i + k * 4] /= pv;
-
-			for (j = k + 1; j < 4; ++j)
-				A[i + j * 4] -= A[i + k * 4] * A[k + j * 4];
-		}
-	}
-
-	return 0;
-}
-
-static void
-inverse_transform(const double *LU, const unsigned *p, float *v)
-{
-	/* Solve A * x = v, when we have P * A = L * U.
-	 * P * A * x = P * v  =>  L * U * x = P * v
-	 * Let U * x = b, then L * b = P * v.
-	 */
-	double b[4];
-	unsigned j;
-
-	/* Forward substitution, column version, solves L * b = P * v */
-	/* The diagonal of L is all ones, and not explicitly stored. */
-	b[0] = v[p[0]];
-	b[1] = (double)v[p[1]] - b[0] * LU[1 + 0 * 4];
-	b[2] = (double)v[p[2]] - b[0] * LU[2 + 0 * 4];
-	b[3] = (double)v[p[3]] - b[0] * LU[3 + 0 * 4];
-	b[2] -= b[1] * LU[2 + 1 * 4];
-	b[3] -= b[1] * LU[3 + 1 * 4];
-	b[3] -= b[2] * LU[3 + 2 * 4];
-
-	/* backward substitution, column version, solves U * y = b */
-#if 1
-	/* hand-unrolled, 25% faster for whole function */
-	b[3] /= LU[3 + 3 * 4];
-	b[0] -= b[3] * LU[0 + 3 * 4];
-	b[1] -= b[3] * LU[1 + 3 * 4];
-	b[2] -= b[3] * LU[2 + 3 * 4];
-
-	b[2] /= LU[2 + 2 * 4];
-	b[0] -= b[2] * LU[0 + 2 * 4];
-	b[1] -= b[2] * LU[1 + 2 * 4];
-
-	b[1] /= LU[1 + 1 * 4];
-	b[0] -= b[1] * LU[0 + 1 * 4];
-
-	b[0] /= LU[0 + 0 * 4];
-#else
-	for (j = 3; j > 0; --j) {
-		unsigned k;
-		b[j] /= LU[j + j * 4];
-		for (k = 0; k < j; ++k)
-			b[k] -= b[j] * LU[k + j * 4];
-	}
-
-	b[0] /= LU[0 + 0 * 4];
-#endif
-
-	/* the result */
-	for (j = 0; j < 4; ++j)
-		v[j] = b[j];
-}
-
-WL_EXPORT int
-weston_matrix_invert(struct weston_matrix *inverse,
-		     const struct weston_matrix *matrix)
-{
-	double LU[16];		/* column-major */
-	unsigned perm[4];	/* permutation */
-	unsigned c;
-
-	if (matrix_invert(LU, perm, matrix) < 0)
-		return -1;
-
-	weston_matrix_init(inverse);
-	for (c = 0; c < 4; ++c)
-		inverse_transform(LU, perm, &inverse->d[c * 4]);
-	inverse->type = matrix->type;
-
-	return 0;
 }
 
 static bool
@@ -521,7 +383,7 @@ get_el(const struct weston_matrix *matrix, int row, int col)
 	assert(row >= 0 && row <= 3);
 	assert(col >= 0 && col <= 3);
 
-	return matrix->d[col * 4 + row];
+	return matrix->M.col[col].el[row];
 }
 
 static bool

clients/calibrator.c
@@ -117,11 +117,11 @@ finish_calibration (struct calibrator *calibrator)
 	 */
 	memset(&m, 0, sizeof(m));
 	for (i = 0; i < (int)ARRAY_LENGTH(test_ratios); i++) {
-		m.d[i] = calibrator->tests[i].clicked_x;
-		m.d[i + 4] = calibrator->tests[i].clicked_y;
-		m.d[i + 8] = 1;
+		m.M.col[0].el[i] = calibrator->tests[i].clicked_x;
+		m.M.col[1].el[i] = calibrator->tests[i].clicked_y;
+		m.M.col[2].el[i] = 1;
 	}
-	m.d[15] = 1;
+	m.M.col[3].el[3] = 1;
 
 	weston_matrix_invert(&inverse, &m);
 
@@ -129,8 +129,8 @@ finish_calibration (struct calibrator *calibrator)
 	memset(&y_calib, 0, sizeof(y_calib));
 
 	for (i = 0; i < (int)ARRAY_LENGTH(test_ratios); i++) {
-		x_calib.f[i] = calibrator->tests[i].drawn_x;
-		y_calib.f[i] = calibrator->tests[i].drawn_y;
+		x_calib.v.el[i] = calibrator->tests[i].drawn_x;
+		y_calib.v.el[i] = calibrator->tests[i].drawn_y;
 	}
 
 	/* Multiples into the vector */
@@ -138,8 +138,8 @@ finish_calibration (struct calibrator *calibrator)
 	weston_matrix_transform(&inverse, &y_calib);
 
 	printf ("Calibration values: %f %f %f %f %f %f\n",
-		x_calib.f[0], x_calib.f[1], x_calib.f[2],
-		y_calib.f[0], y_calib.f[1], y_calib.f[2]);
+		x_calib.v.el[0], x_calib.v.el[1], x_calib.v.el[2],
+		y_calib.v.el[0], y_calib.v.el[1], y_calib.v.el[2]);
 
 	exit(0);
 }

clients/simple-dmabuf-egl.c
@@ -815,7 +815,7 @@ render(struct window *window, struct buffer *buffer)
 
 	glUniform1f(window->gl.offset_uniform, offset);
 	glUniformMatrix4fv(window->gl.reflection_uniform, 1, GL_FALSE,
-			   (GLfloat *) reflection.d);
+			   (GLfloat *) reflection.M.colmaj);
 
 	glClearColor(0.0,0.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -864,7 +864,7 @@ render_mandelbrot(struct window *window, struct buffer *buffer)
 
 	glUniform1f(window->gl.offset_uniform, offset);
 	glUniformMatrix4fv(window->gl.reflection_uniform, 1, GL_FALSE,
-			   (GLfloat *) reflection.d);
+			   (GLfloat *) reflection.M.colmaj);
 
 	glClearColor(0.6, 0.6, 0.6, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);

clients/simple-egl.c
@@ -940,10 +940,10 @@ redraw(struct window *window)
 		angle = ((time - window->initial_frame_time) / speed_div)
 			% 360 * M_PI / 180.0;
 	}
-	rotation.d[0] =   cos(angle);
-	rotation.d[2] =   sin(angle);
-	rotation.d[8] =  -sin(angle);
-	rotation.d[10] =  cos(angle);
+	rotation.M.col[0].el[0] =  cos(angle);
+	rotation.M.col[0].el[2] =  sin(angle);
+	rotation.M.col[2].el[0] = -sin(angle);
+	rotation.M.col[2].el[2] =  cos(angle);
 
 	switch (window->buffer_transform) {
 	case WL_OUTPUT_TRANSFORM_FLIPPED:
@@ -982,7 +982,7 @@ redraw(struct window *window)
 	glViewport(0, 0, window->buffer_size.width, window->buffer_size.height);
 
 	glUniformMatrix4fv(window->gl.rotation_uniform, 1, GL_FALSE,
-			   (GLfloat *) rotation.d);
+			   (GLfloat *) rotation.M.colmaj);
 
 	if (window->opaque || window->fullscreen)
 		glClearColor(0.0, 0.0, 0.0, 1);

clients/touch-calibrator.c
@@ -330,9 +330,9 @@ compute_calibration(struct calibrator *cal, float *result)
 	 */
 	weston_matrix_init(&m);
 	for (i = 0; i < 3; i++) {
-		m.d[i + 0] = cal->samples[i].touched.x;
-		m.d[i + 4] = cal->samples[i].touched.y;
-		m.d[i + 8] = 1.0f;
+		m.M.col[0].el[i] = cal->samples[i].touched.x;
+		m.M.col[1].el[i] = cal->samples[i].touched.y;
+		m.M.col[2].el[i] = 1.0f;
 	}
 	m.type = WESTON_MATRIX_TRANSFORM_OTHER;
 
@@ -342,20 +342,20 @@ compute_calibration(struct calibrator *cal, float *result)
 	}
 
 	for (i = 0; i < 3; i++) {
-		x_calib.f[i] = cal->samples[i].drawn_cal.x;
-		y_calib.f[i] = cal->samples[i].drawn_cal.y;
+		x_calib.v.el[i] = cal->samples[i].drawn_cal.x;
+		y_calib.v.el[i] = cal->samples[i].drawn_cal.y;
 	}
-	x_calib.f[3] = 0.0f;
-	y_calib.f[3] = 0.0f;
+	x_calib.v.el[3] = 0.0f;
+	y_calib.v.el[3] = 0.0f;
 
 	/* Multiples into the vector */
 	weston_matrix_transform(&inverse, &x_calib);
 	weston_matrix_transform(&inverse, &y_calib);
 
 	for (i = 0; i < 3; i++)
-		result[i] = x_calib.f[i];
+		result[i] = x_calib.v.el[i];
 	for (i = 0; i < 3; i++)
-		result[i + 3] = y_calib.f[i];
+		result[i + 3] = y_calib.v.el[i];
 
 	return 0;
 }

clients/window.c
@@ -1842,8 +1842,9 @@ widget_cairo_update_transform(struct widget *widget, cairo_t *cr)
 				     surface->allocation.width,
 				     surface->allocation.height,
 				     surface->buffer_scale);
-	cairo_matrix_init(&m, matrix.d[0], matrix.d[1], matrix.d[4],
-			  matrix.d[5], matrix.d[12], matrix.d[13]);
+	cairo_matrix_init(&m, matrix.M.col[0].x, matrix.M.col[0].y,
+			      matrix.M.col[1].x, matrix.M.col[1].y,
+			      matrix.M.col[3].x, matrix.M.col[3].y);
 	cairo_transform(cr, &m);
 }
 

include/libweston/matrix.h
@@ -31,6 +31,7 @@
 #include <stdbool.h>
 
 #include <wayland-server-protocol.h>
+#include <libweston/linalg-types.h>
 
 #ifdef  __cplusplus
 extern "C" {
@@ -44,12 +45,12 @@ enum weston_matrix_transform_type {
 };
 
 struct weston_matrix {
-	float d[16];
+	struct weston_mat4f M;
 	unsigned int type;
 };
 
 struct weston_vector {
-	float f[4];
+	struct weston_vec4f v;
 };
 
 /** Arbitrary coordinates in any space */

ivi-shell/ivi-layout.c
@@ -463,33 +463,33 @@ calc_inverse_matrix_transform(const struct weston_matrix *matrix,
 	}
 
 	/* The vectors and matrices involved will always produce f[3] == 1.0. */
-	top_left.f[0] = rect_input->x;
-	top_left.f[1] = rect_input->y;
-	top_left.f[2] = 0.0f;
-	top_left.f[3] = 1.0f;
+	top_left.v.el[0] = rect_input->x;
+	top_left.v.el[1] = rect_input->y;
+	top_left.v.el[2] = 0.0f;
+	top_left.v.el[3] = 1.0f;
 
-	bottom_right.f[0] = rect_input->x + rect_input->width;
-	bottom_right.f[1] = rect_input->y + rect_input->height;
-	bottom_right.f[2] = 0.0f;
-	bottom_right.f[3] = 1.0f;
+	bottom_right.v.el[0] = rect_input->x + rect_input->width;
+	bottom_right.v.el[1] = rect_input->y + rect_input->height;
+	bottom_right.v.el[2] = 0.0f;
+	bottom_right.v.el[3] = 1.0f;
 
 	weston_matrix_transform(&m, &top_left);
 	weston_matrix_transform(&m, &bottom_right);
 
-	if (top_left.f[0] < bottom_right.f[0]) {
-		rect_output->x = floorf(top_left.f[0]);
-		rect_output->width = ceilf(bottom_right.f[0] - rect_output->x);
+	if (top_left.v.el[0] < bottom_right.v.el[0]) {
+		rect_output->x = floor(top_left.v.el[0]);
+		rect_output->width = ceil(bottom_right.v.el[0] - rect_output->x);
 	} else {
-		rect_output->x = floorf(bottom_right.f[0]);
-		rect_output->width = ceilf(top_left.f[0] - rect_output->x);
+		rect_output->x = floor(bottom_right.v.el[0]);
+		rect_output->width = ceil(top_left.v.el[0] - rect_output->x);
 	}
 
-	if (top_left.f[1] < bottom_right.f[1]) {
-		rect_output->y = floorf(top_left.f[1]);
-		rect_output->height = ceilf(bottom_right.f[1] - rect_output->y);
+	if (top_left.v.el[1] < bottom_right.v.el[1]) {
+		rect_output->y = floor(top_left.v.el[1]);
+		rect_output->height = ceil(bottom_right.v.el[1] - rect_output->y);
 	} else {
-		rect_output->y = floorf(bottom_right.f[1]);
-		rect_output->height = ceilf(top_left.f[1] - rect_output->y);
+		rect_output->y = floor(bottom_right.v.el[1]);
+		rect_output->height = ceil(top_left.v.el[1] - rect_output->y);
 	}
 
 	ivi_rectangle_intersect(rect_output, boundingbox, rect_output);

commit 135b95808c32b77706d11cff363da7901d916c27

shared: fix matrix type computations
When I changed the weston_matrix implementation to linalg-4.h, I broke
the type computations: they were getting reset instead of accumulated.
This manifested with the desktop-shell feature where one can arbitrarily
rotate the windows. A rotated window triggered an incorrect matrix type,
which then did not ignore the surface opaque region as it should have.
That caused rendering artifacts on all renderers.

Fixes: 3fefb5ba4468c7995abd3c4e878c526e386b224e
Fixes: https://gitlab.freedesktop.org/wayland/weston/-/issues/1031

Signed-off-by: Pekka Paalanen <pekka.paalanen@collabora.com>

shared/matrix.c
@@ -64,21 +64,21 @@ WL_EXPORT void
 weston_matrix_translate(struct weston_matrix *matrix, float x, float y, float z)
 {
 	matrix->M = weston_m4f_mul_m4f(weston_m4f_translation(x, y, z), matrix->M);
-	matrix->type = WESTON_MATRIX_TRANSFORM_TRANSLATE;
+	matrix->type |= WESTON_MATRIX_TRANSFORM_TRANSLATE;
 }
 
 WL_EXPORT void
 weston_matrix_scale(struct weston_matrix *matrix, float x, float y,float z)
 {
 	matrix->M = weston_m4f_mul_m4f(weston_m4f_scaling(x, y, z), matrix->M);
-	matrix->type = WESTON_MATRIX_TRANSFORM_SCALE;
+	matrix->type |= WESTON_MATRIX_TRANSFORM_SCALE;
 }
 
 WL_EXPORT void
 weston_matrix_rotate_xy(struct weston_matrix *matrix, float cos, float sin)
 {
 	matrix->M = weston_m4f_mul_m4f(weston_m4f_rotation_xy(cos, sin), matrix->M);
-	matrix->type = WESTON_MATRIX_TRANSFORM_ROTATE;
+	matrix->type |= WESTON_MATRIX_TRANSFORM_ROTATE;
 }
 
 /* v <- m * v */