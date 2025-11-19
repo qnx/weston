qnx/weston

commit abfe874a5127d86ace5531a3f936335d99628691

core: Don't rebuild view list on surface-local changes
If a surface changes size or opaque region, or a view changes position,
we don't need to rebuild the entire view list from scratch: we can just
rebuild what's changed and nothing else.

Signed-off-by: Daniel Stone <daniels@collabora.com>

libweston/compositor.c
@@ -111,9 +111,6 @@ static void
 subsurface_committed(struct weston_surface *surface,
 		     struct weston_coord_surface new_origin);
 
-static void
-weston_view_geometry_dirty_internal(struct weston_view *view);
-
 static bool
 weston_view_is_fully_blended(struct weston_view *ev,
 			     pixman_region32_t *region);
@@ -1884,7 +1881,7 @@ weston_view_update_transform(struct weston_view *view)
 	}
 }
 
-static void
+void
 weston_view_geometry_dirty_internal(struct weston_view *view)
 {
 	struct weston_view *child;
@@ -1987,7 +1984,8 @@ weston_view_set_rel_position(struct weston_view *view,
 		return;
 
 	view->geometry.pos_offset = offset.c;
-	weston_view_geometry_dirty(view);
+	weston_view_geometry_dirty_internal(view);
+	weston_view_update_transform(view);
 }
 
 WL_EXPORT void
@@ -2002,7 +2000,8 @@ weston_view_set_position(struct weston_view *view,
 		return;
 
 	view->geometry.pos_offset = pos.c;
-	weston_view_geometry_dirty(view);
+	weston_view_geometry_dirty_internal(view);
+	weston_view_update_transform(view);
 }
 
 WL_EXPORT void
@@ -2373,7 +2372,7 @@ surface_set_size(struct weston_surface *surface, int32_t width, int32_t height)
 	surface->height = height;
 
 	wl_list_for_each(view, &surface->views, surface_link)
-		weston_view_geometry_dirty(view);
+		weston_view_geometry_dirty_internal(view);
 }
 
 WL_EXPORT void

libweston/libweston-internal.h
@@ -555,6 +555,9 @@ weston_view_takes_input_at_point(struct weston_view *view,
 				 struct weston_coord_surface surf_pos);
 
 void
+weston_view_geometry_dirty_internal(struct weston_view *view);
+
+void
 weston_paint_node_move_to_plane(struct weston_paint_node *pnode,
 				struct weston_plane *plane);
 void

libweston/surface-state.c
@@ -340,8 +340,10 @@ weston_surface_apply_state(struct weston_surface *surface,
 
 		if (!pixman_region32_equal(&opaque, &surface->opaque)) {
 			pixman_region32_copy(&surface->opaque, &opaque);
-			wl_list_for_each(view, &surface->views, surface_link)
-				weston_view_geometry_dirty(view);
+			wl_list_for_each(view, &surface->views, surface_link) {
+				weston_view_geometry_dirty_internal(view);
+				weston_view_update_transform(view);
+			}
 		}
 
 		pixman_region32_fini(&opaque);

commit 881ac6a5cf221c92f734282db92081c01ca93c08

compositor: De-couple paint node ordered list from the view build list
This de-couples the compositor view build list from the paint node
ordered list to allow a more finer grain over what lists we need to
rebuild upon repaints.

As a consequence to that this avoids a trip over from the compositor
when paint nodes are destroyed and no longer re-created upon rebuilding
the view list.

Fixes: #1070
Fixes: abfe874a ("core: Don't rebuild view list on surface-local changes")

Signed-off-by: Marius Vlad <marius.vlad@collabora.com>

include/libweston/libweston.h
@@ -547,6 +547,9 @@ struct weston_output {
 	 * output to display.
 	 */
 	bool ready;
+
+	/** Needs to rebuild the paint node z ordered list */
+	bool paint_node_list_needs_rebuild;
 };
 
 enum weston_pointer_motion_mask {

libweston/compositor.c
@@ -1536,6 +1536,23 @@ weston_surface_assign_output(struct weston_surface *es)
 	weston_surface_update_preferred_color_profile(es);
 }
 
+static void
+weston_view_set_output_mask(struct weston_view *ev, uint32_t new_mask)
+{
+	struct weston_output *output;
+	struct weston_compositor *ec = ev->surface->compositor;
+	uint32_t old_mask = ev->output_mask;
+
+	/* verify which outputs have actually changed to trigger a paint
+	 * re-order build on them */
+	wl_list_for_each(output, &ec->output_list, link) {
+		if ((1u << output->id) & (old_mask ^ new_mask))
+			output->paint_node_list_needs_rebuild = true;
+	}
+
+	ev->output_mask = new_mask;
+}
+
 /** Recalculate which output(s) the view is displayed on
  *
  * \param ev  The view to remap to outputs
@@ -1599,8 +1616,8 @@ weston_view_assign_output(struct weston_view *ev)
 	}
 	pixman_region32_fini(&region);
 
+	weston_view_set_output_mask(ev, mask);
 	weston_view_set_output(ev, new_output);
-	ev->output_mask = mask;
 
 	weston_surface_assign_output(ev->surface);
 
@@ -2499,7 +2516,7 @@ weston_view_unmap(struct weston_view *view)
 	view->layer_link.layer = NULL;
 	wl_list_remove(&view->link);
 	wl_list_init(&view->link);
-	view->output_mask = 0;
+	weston_view_set_output_mask(view, 0x0);
 	weston_surface_assign_output(view->surface);
 
 	if (!weston_surface_is_mapped(view->surface)) {
@@ -3414,6 +3431,8 @@ weston_output_build_z_order_list(struct weston_compositor *compositor,
 		pnode = view_ensure_paint_node(view, output);
 		add_to_z_order_list(output, pnode);
 	}
+
+	output->paint_node_list_needs_rebuild = false;
 }
 
 static void
@@ -3434,8 +3453,17 @@ weston_compositor_build_view_list(struct weston_compositor *compositor)
 		}
 	}
 
+	/* while rebuilding the view list would implicitly need a paint node
+	 * rebuild list as well, the other way around is not true.
+	 * Separating these two allows other call sites to just issue a paint
+	 * node reorder without triggering a rebuild of the view list, allowing
+	 * to decouple the two lists rebuilds.
+	 *
+	 * Here we just mark it accordingly while another spot is when the
+	 * view's output_mask has changed.
+	 */
 	wl_list_for_each(output, &compositor->output_list, link)
-		weston_output_build_z_order_list(compositor, output);
+		output->paint_node_list_needs_rebuild = true;
 
 	compositor->view_list_needs_rebuild = false;
 }
@@ -3580,6 +3608,9 @@ weston_output_repaint(struct weston_output *output)
 		weston_compositor_build_view_list(ec);
 		output->paint_node_changes = WESTON_PAINT_NODE_ALL_DIRTY;
 	}
+
+	if (output->paint_node_list_needs_rebuild)
+		weston_output_build_z_order_list(ec, output);
 
 	/* If the scene graph is empty, we could end up passing a buffer
 	 * we've never drawn into to a hardware plane later. If that hardware