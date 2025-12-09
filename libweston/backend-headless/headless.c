qnx/weston

commit 0126a5b4fc03c3ac7f023d87303bf09f7d494ac1

backend-headless: Add an option to enable a fake seat
When running headless, weston will not expose a wl_seat.

This was removed with commit a1046adc ("compositor-headless: do not
create a seat").

However, some applications, namely GTK3 based, will log a warning when
there is no wl_seat:

 | gdk_seat_get_keyboard assertion GDK_IS_SEAT(seat) failed

While this is arguably a bug in GTK3 which should not complain with a
legit setup, that breaks the CI of those projects when using Weston,
while most of the other Wayland compositors will create a fake seat when
running headless, making weston the odd ball there.

This changes adds a new option "--fake-seat" that will instruct weston
to create a seat when running headless. The default remains not to
create a seat though, so backward compatibility is preserved.

This partially reverts commit a1046adc6603054187dc10ecaeee0ce271fcedec.

See-also: https://gitlab.freedesktop.org/ofourdan/xwayland-run/-/issues/12
Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>

libweston/backend-headless/headless.c
@@ -70,6 +70,8 @@ struct headless_backend {
 
 	int refresh;
 	bool repaint_only_on_capture;
+
+	bool use_fake_seat;
 };
 
 struct headless_head {
@@ -598,6 +600,25 @@ headless_head_destroy(struct weston_head *base)
 	free(head);
 }
 
+static bool
+headless_input_create(struct headless_backend *b)
+{
+	weston_seat_init(&b->fake_seat, b->compositor, "default");
+
+	weston_seat_init_pointer(&b->fake_seat);
+
+	if (weston_seat_init_keyboard(&b->fake_seat, NULL) < 0)
+		return false;
+
+	return true;
+}
+
+static void
+headless_input_destroy(struct headless_backend *b)
+{
+	weston_seat_release(&b->fake_seat);
+}
+
 static void
 headless_destroy(struct weston_backend *backend)
 {
@@ -614,6 +635,9 @@ headless_destroy(struct weston_backend *backend)
 
 	if (b->theme)
 		theme_destroy(b->theme);
+
+	if (b->use_fake_seat)
+		headless_input_destroy(b);
 
 	free(b->formats);
 	free(b);
@@ -645,6 +669,12 @@ headless_backend_create(struct weston_compositor *compositor,
 
 	b->base.supported_presentation_clocks =
 			WESTON_PRESENTATION_CLOCKS_SOFTWARE;
+
+	if (config->fake_seat) {
+		b->use_fake_seat = headless_input_create(b);
+		if (!b->use_fake_seat)
+			goto err_free;
+	}
 
 	b->base.destroy = headless_destroy;
 	b->base.create_output = headless_output_create;
@@ -744,6 +774,8 @@ err_input:
 	if (b->theme)
 		theme_destroy(b->theme);
 err_free:
+	if (b->use_fake_seat)
+		headless_input_destroy(b);
 	wl_list_remove(&b->base.link);
 	free(b);
 	return NULL;
@@ -753,6 +785,7 @@ static void
 config_init_to_defaults(struct weston_headless_backend_config *config)
 {
 	config->refresh = DEFAULT_OUTPUT_REPAINT_REFRESH;
+	config->fake_seat = FALSE;
 }
 
 WL_EXPORT int

frontend/main.c
@@ -771,6 +771,7 @@ usage(int error_code)
 		"  --use-vulkan\t\tUse the Vulkan renderer (deprecated alias for --renderer=vulkan)\n"
 		"  --no-outputs\t\tDo not create any virtual outputs\n"
 		"  --refresh-rate=RATE\tThe output refresh rate (in mHz)\n"
+		"  --fake-seat\t\tUse a fake seat for compatibility\n"
 		"\n");
 #endif
 
@@ -4328,6 +4329,7 @@ load_headless_backend(struct weston_compositor *c,
 		{ WESTON_OPTION_STRING, "transform", 0, &transform },
 		{ WESTON_OPTION_BOOLEAN, "no-outputs", 0, &no_outputs },
 		{ WESTON_OPTION_INTEGER, "refresh-rate", 0, &config.refresh },
+		{ WESTON_OPTION_BOOLEAN, "fake-seat", 0, &config.fake_seat },
 	};
 	config.refresh = -1;
 

include/libweston/backend-headless.h
@@ -49,6 +49,11 @@ struct weston_headless_backend_config {
 	 * mHz to 1,000,000 mHz. 0 is a special value that triggers repaints
 	 * only on capture requests, not on damages. */
 	int refresh;
+
+	/** Create a fake seat, some clients may complain without a wl_seat.
+	 * The default is not to create a wl_seat.
+	 */
+	bool fake_seat;
 };
 
 #ifdef  __cplusplus

commit 85315c484185f88d5119ccf757e60f10afcddfa2

backend-headless: fix undeclared FALSE identifier
It looks like FALSE is only used in a few files in Weston, whereas false is much
more commonly used.  Presumably FALSE comes from some library not included in
the headless backend (at least in my case).

	FAILED: [code=1] libweston/backend-headless/headless-backend.so.p/headless.c.o
	gcc -Ilibweston/backend-headless/headless-backend.so.p -Ilibweston/backend-headless -I../../home/qyliss/src/weston/libweston/backend-headless -I. -I../../home/qyliss/src/weston -Iinclude -I../../home/qyliss/src/weston/include -Ilibweston -I../../home/qyliss/src/weston/libweston -Iprotocol -I/nix/store/mvhj7fm6bkmz6ismdp8vcmwn34w94b8n-libglvnd-1.7.0-dev/include -I/nix/store/v7jn3cy1wjaf9pm60ix9y6dfma0bj4a3-wayland-1.24.0-dev/include -I/nix/store/xi04j07bl6bbckky5n8a1k06spxxfg9i-pixman-0.46.4/include/pixman-1 -I/nix/store/xvzhnfwxvl480qpjxm83wwhd287b3lgm-libxkbcommon-1.11.0-dev/include -I/nix/store/22qgb8p593613861117ay4gzmf9b7rns-libdrm-2.4.125-dev/include -I/nix/store/22qgb8p593613861117ay4gzmf9b7rns-libdrm-2.4.125-dev/include/libdrm -I/nix/store/zl58kqaf677skihj840rf05svsmrhmmc-cairo-1.18.4-dev/include/cairo -I/nix/store/6sscd9xxffhjrci2xbw9yv8inc2rc0ry-freetype-2.13.3-dev/include/freetype2 -I/nix/store/6sscd9xxffhjrci2xbw9yv8inc2rc0ry-freetype-2.13.3-dev/include -I/nix/store/z88mxig65mxgyn1yxsz92m4w1rn51aci-libpng-apng-1.6.50-dev/include/libpng16 -fdiagnostics-color=always -D_FILE_OFFSET_BITS=64 -Wall -Winvalid-pch -Wextra -Wpedantic -std=gnu11 -O0 -g -Wmissing-prototypes -Wno-unused-parameter -Wno-shift-negative-value -Wno-missing-field-initializers -Wno-pedantic -Wundef -fvisibility=hidden -Wmissing-prototypes -Wno-unused-parameter -Wno-shift-negative-value -Wno-missing-field-initializers -Wno-pedantic -Wundef -fvisibility=hidden -fPIC -MD -MQ libweston/backend-headless/headless-backend.so.p/headless.c.o -MF libweston/backend-headless/headless-backend.so.p/headless.c.o.d -o libweston/backend-headless/headless-backend.so.p/headless.c.o -c ../../home/qyliss/src/weston/libweston/backend-headless/headless.c
	../../home/qyliss/src/weston/libweston/backend-headless/headless.c: In function ‘config_init_to_defaults’:
	../../home/qyliss/src/weston/libweston/backend-headless/headless.c:788:29: error: ‘FALSE’ undeclared (first use in this function)
	  788 |         config->fake_seat = FALSE;
	      |                             ^~~~~
	../../home/qyliss/src/weston/libweston/backend-headless/headless.c:788:29: note: each undeclared identifier is reported only once for each function it appears in

Fixes: 0126a5b4 ("backend-headless: Add an option to enable a fake seat")

libweston/backend-headless/headless.c
@@ -785,7 +785,7 @@ static void
 config_init_to_defaults(struct weston_headless_backend_config *config)
 {
 	config->refresh = DEFAULT_OUTPUT_REPAINT_REFRESH;
-	config->fake_seat = FALSE;
+	config->fake_seat = false;
 }
 
 WL_EXPORT int