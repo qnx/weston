2f5ad2f046197d4d2abb61c6e7f497c4dbfcd5ea
Marius Vlad <marius.vlad@collabora.com>
2026-01-06 16:48:31 +0200

    simple-timing: Use the same name as client name for app_id/title
    Signed-off-by: Marius Vlad <marius.vlad@collabora.com>

--- clients/simple-timing.c
+++ clients/simple-timing.c
@@ -392,9 +392,9 @@ create_window(struct display *display, int width, int height)
 	xdg_toplevel_add_listener(window->xdg_toplevel,
 				  &xdg_toplevel_listener, window);
 
-	xdg_toplevel_set_title(window->xdg_toplevel, "simple-shm");
+	xdg_toplevel_set_title(window->xdg_toplevel, "simple-timing");
 	xdg_toplevel_set_app_id(window->xdg_toplevel,
-			"org.freedesktop.weston.simple-shm");
+			"org.freedesktop.weston.simple-timing");
 
 	wl_surface_commit(window->surface);
 	window->wait_for_configure = true;
