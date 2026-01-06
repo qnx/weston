qnx/weston

commit 65227cb7d42a064031eeb08cb37e92bfc92b7815

compositor: Round repaint times down to possible presentation times
Currently this shouldn't result in any change, since the times we pass in
are real presentation times.

Later when commit-timing lets applications pass in arbitrary times, it
will be more interesting.

Signed-off-by: Derek Foreman <derek.foreman@collabora.com>

libweston/compositor.c
@@ -3921,6 +3921,10 @@ weston_output_repaint_from_present(const struct weston_output *output,
 				   const struct timespec *present_time)
 {
 	struct timespec repaint_time;
+	int64_t time_since;
+	int64_t frames_since;
+	int refresh_nsec = millihz_to_nsec(output->current_mode->refresh);
+	struct timespec actual_present_time;
 	bool late = false;
 
 	if (timespec_sub_to_nsec(now, present_time) > 0)
@@ -3937,8 +3941,16 @@ weston_output_repaint_from_present(const struct weston_output *output,
 	    output->vrr_mode == WESTON_VRR_MODE_GAME)
 		return late ? *now : *present_time;
 
-	timespec_add_msec(&repaint_time, present_time,
-			  -weston_output_repaint_msec(output));
+	/* The provided time might be in the middle of a frame, so round it
+	 * back to the start of the frame that time is in.
+	 */
+	time_since = timespec_sub_to_nsec(present_time, &output->frame_time);
+	time_since--;
+	frames_since = time_since / refresh_nsec;
+	timespec_add_nsec(&actual_present_time, &output->frame_time, refresh_nsec * (frames_since + 1));
+
+	/* Subtract the "repaint window" time to get the deadline for the presentation time */
+	timespec_add_msec(&repaint_time, &actual_present_time, -weston_output_repaint_msec(output));
 
 	return repaint_time;
 }

commit 1f642c8c105df1f698f01e473ccf32038e597971

compositor: Fix failure to start with multiple outputs
The change in 65227cb7d42 is actually destructive, as it causes multiple
drm-backend heads to no longer be repainted at the exact same time at
startup.

This frequently leads to a failure to flip when only one head is properly
set up and others have stale state.

Force stampless finish_frame to skip any time adjustments to restore proper
behaviour.

Fixes: 65227cb7 (compositor: Round repaint times down to possible presentation times)
Signed-off-by: Derek Foreman <derek.foreman@collabora.com>

libweston/compositor.c
@@ -4309,11 +4309,20 @@ weston_output_finish_frame(struct weston_output *output,
 
 	/* If we haven't been supplied any timestamp at all, we don't have a
 	 * timebase to work against, so any delay just wastes time. Push a
-	 * repaint as soon as possible so we can get on with it. */
+	 * repaint as soon as possible so we can get on with it.
+	 *
+	 * We must not delay because the compositor hits this path at startup,
+	 * and the drm backend must have all its outputs repaint at the same
+	 * time (regardless of their refresh rates) for the first repaint,
+	 * or the combination of new and stale state will prevent the flip.
+	 */
 	if (!stamp) {
 		output->next_present = now;
+		output->next_repaint = now;
 		output->frame_flags = 0;
-		goto out;
+		output->repaint_status = REPAINT_SCHEDULED;
+		weston_repaint_timer_arm(compositor);
+		return;
 	}
 
 	vblank_monotonic = convert_presentation_time_now(compositor,