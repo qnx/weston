8ba7fb819532d9771f09a3fd030aa2496e88f944
Marius Vlad <marius.vlad@collabora.com>
2026-01-06 16:52:32 +0200

    simple-timing: Use don't use the frame callback mechanism
    commit-timing uses presentation feedback to drive its repaint loop, no
    need to keep the frame callback.
    
    Signed-off-by: Marius Vlad <marius.vlad@collabora.com>

--- clients/simple-timing.c
+++ clients/simple-timing.c
@@ -89,7 +89,6 @@ struct window {
 	struct xdg_surface *xdg_surface;
 	struct xdg_toplevel *xdg_toplevel;
 	struct wl_list buffer_list;
-	struct wl_callback *callback;
 	struct wp_fifo_v1 *fifo;
 	struct wp_commit_timer_v1 *commit_timer;
 	bool wait_for_configure;
@@ -363,7 +362,6 @@ create_window(struct display *display, int width, int height)
 	if (!window)
 		return NULL;
 
-	window->callback = NULL;
 	window->display = display;
 	window->width = width;
 	window->height = height;
@@ -409,9 +407,6 @@ static void
 destroy_window(struct window *window)
 {
 	struct buffer *buffer, *buffer_next;
-
-	if (window->callback)
-		wl_callback_destroy(window->callback);
 
 	wl_list_for_each_safe(buffer, buffer_next,
 			      &window->buffer_list, buffer_link)
