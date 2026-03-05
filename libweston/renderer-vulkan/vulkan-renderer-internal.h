qnx/weston

commit 8f56d03d4b0f77da7af921ad9cf607e78d3f5953

libweston: Vulkan renderer
A Vulkan renderer for weston, based on the GL renderer.
The goal is to impose the least requirements as possible on Vulkan
implementations, as to allow even Vulkan 1.0 (or early development)
drivers to run a Wayland compositor. Any additional features or
extensions are made optional if possible.
Currently supports drm, wayland, x11 and headless backends.
As of this implementation, this is still considered an experimental
renderer.

Signed-off-by: Erico Nunes <nunes.erico@gmail.com>

libweston/renderer-vulkan/vulkan-renderer-internal.h
@@ -1,0 +1,203 @@
+/*
+ * Copyright © 2025 Erico Nunes
+ *
+ * based on gl-renderer-internal.h:
+ * Copyright © 2019 Collabora, Ltd.
+ * Copyright © 2019 Harish Krupo
+ * Copyright © 2019 Intel Corporation
+ * Copyright 2021 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef VULKAN_RENDERER_INTERNAL_H
+#define VULKAN_RENDERER_INTERNAL_H
+
+#include <stdbool.h>
+#include <time.h>
+
+#include <wayland-util.h>
+#include <vulkan/vulkan.h>
+#include "shared/helpers.h"
+#include "libweston/libweston.h"
+#include "libweston/libweston-internal.h"
+#include <xcb/xcb.h>
+
+#define VK_USE_PLATFORM_XCB_KHR
+#define VK_USE_PLATFORM_WAYLAND_KHR
+#include <vulkan/vulkan_wayland.h>
+#include <vulkan/vulkan_xcb.h>
+
+#define MAX_NUM_IMAGES 5
+#define MAX_CONCURRENT_FRAMES 2
+
+enum vulkan_pipeline_texture_variant {
+	PIPELINE_VARIANT_NONE = 0,
+/* Keep the following in sync with Vulkan shader.frag. */
+	PIPELINE_VARIANT_RGBA     = 1,
+	PIPELINE_VARIANT_RGBX     = 2,
+	PIPELINE_VARIANT_SOLID    = 3,
+	PIPELINE_VARIANT_EXTERNAL = 4,
+};
+
+struct vulkan_pipeline_requirements
+{
+	unsigned texcoord_input:1; /* enum vulkan_shader_texcoord_input */
+	unsigned variant:4; /* enum vulkan_pipeline_texture_variant */
+	bool input_is_premult:1;
+	bool blend:1;
+	VkRenderPass renderpass;
+};
+
+struct vulkan_pipeline_config {
+	struct vulkan_pipeline_requirements req;
+
+	struct weston_matrix projection;
+	struct weston_matrix surface_to_buffer;
+	float view_alpha;
+	float unicolor[4];
+};
+
+
+/* Keep the following in sync with vertex.glsl. */
+enum vulkan_shader_texcoord_input {
+	SHADER_TEXCOORD_INPUT_ATTRIB = 0,
+	SHADER_TEXCOORD_INPUT_SURFACE,
+};
+
+struct vulkan_pipeline {
+	struct vulkan_pipeline_requirements key;
+
+	struct wl_list link; /* vulkan_renderer::pipeline_list */
+	struct timespec last_used;
+
+	VkDescriptorSetLayout descriptor_set_layout;
+
+	VkPipeline pipeline;
+	VkPipelineLayout pipeline_layout;
+};
+
+struct vulkan_renderer_texture_image {
+	VkImage image;
+	VkDeviceMemory memory;
+	VkImageView image_view;
+
+	VkBuffer staging_buffer;
+	VkDeviceMemory staging_memory;
+	void *staging_map;
+
+	VkCommandBuffer upload_cmd;
+	VkFence upload_fence;
+};
+
+struct vulkan_renderer {
+	struct weston_renderer base;
+	struct weston_compositor *compositor;
+
+	bool has_wayland_surface;
+	bool has_xcb_surface;
+	VkInstance inst;
+
+	VkPhysicalDevice phys_dev;
+	VkQueue queue;
+	uint32_t queue_family;
+
+	bool has_incremental_present;
+	bool has_image_drm_format_modifier;
+	bool has_external_semaphore_fd;
+	bool has_physical_device_drm;
+	bool has_external_memory_dma_buf;
+	bool has_queue_family_foreign;
+	bool semaphore_import_export;
+	VkDevice dev;
+
+	VkCommandPool cmd_pool;
+
+	int drm_fd; /* drm device fd */
+	struct weston_drm_format_array supported_formats;
+	struct wl_list dmabuf_images;
+	struct wl_list dmabuf_formats;
+
+	struct wl_signal destroy_signal;
+	struct wl_list pipeline_list;
+	struct dmabuf_allocator *allocator;
+
+	PFN_vkCreateWaylandSurfaceKHR create_wayland_surface;
+	PFN_vkCreateXcbSurfaceKHR create_xcb_surface;
+	PFN_vkGetPhysicalDeviceWaylandPresentationSupportKHR get_wayland_presentation_support;
+	PFN_vkGetPhysicalDeviceXcbPresentationSupportKHR get_xcb_presentation_support;
+
+	PFN_vkGetImageMemoryRequirements2KHR get_image_memory_requirements2;
+	PFN_vkGetMemoryFdPropertiesKHR get_memory_fd_properties;
+	PFN_vkGetSemaphoreFdKHR get_semaphore_fd;
+	PFN_vkImportSemaphoreFdKHR import_semaphore_fd;
+
+	/* This can be removed if a different shader is defined
+	 * to avoid requiring a valid sampler descriptor to run
+	 * for solids */
+	struct {
+		struct vulkan_renderer_texture_image image;
+		VkSampler sampler;
+	} dummy;
+};
+
+static inline struct vulkan_renderer *
+get_renderer(struct weston_compositor *ec)
+{
+	return (struct vulkan_renderer *)ec->renderer;
+}
+
+static inline void pnext(void *base, void *next)
+{
+	VkBaseOutStructure *b = base;
+	VkBaseOutStructure *n = next;
+	n->pNext = b->pNext;
+	b->pNext = n;
+}
+
+static inline void _check_vk_success(const char *file, int line, const char *func,
+				     VkResult result, const char *vk_func)
+{
+	if (result == VK_SUCCESS)
+		return;
+
+	weston_log("%s %d %s Error: %s failed with VkResult %d\n", file, line, func, vk_func, result);
+	abort();
+}
+#define check_vk_success(result, vk_func) \
+	_check_vk_success(__FILE__, __LINE__, __func__, (result), (vk_func))
+
+void
+vulkan_pipeline_destroy(struct vulkan_renderer *vr, struct vulkan_pipeline *pipeline);
+
+void
+vulkan_renderer_pipeline_list_destroy(struct vulkan_renderer *vr);
+
+struct vulkan_pipeline *
+vulkan_renderer_get_pipeline(struct vulkan_renderer *vr,
+			     const struct vulkan_pipeline_requirements *reqs);
+
+bool
+vulkan_renderer_query_dmabuf_format(struct vulkan_renderer *vr,
+				    const struct pixel_format_info *format);
+
+#endif /* VULKAN_RENDERER_INTERNAL_H */

frontend/config-helpers.c
@@ -70,6 +70,7 @@ struct {
 } renderer_name_map[] = {
 	{ "auto", WESTON_RENDERER_AUTO },
 	{ "gl", WESTON_RENDERER_GL },
+	{ "vulkan", WESTON_RENDERER_VULKAN },
 	{ "noop", WESTON_RENDERER_NOOP },
 	{ "pixman", WESTON_RENDERER_PIXMAN },
 };

frontend/main.c
@@ -711,6 +711,9 @@ usage(int error_code)
 #if defined(ENABLE_EGL)
 			"\t\t\t\tgl\tOpenGL ES\n"
 #endif
+#if defined(ENABLE_VULKAN)
+			"\t\t\t\tvulkan\tVulkan\n"
+#endif
 			"\t\t\t\tnoop\tNo-op renderer for testing only\n"
 			"\t\t\t\tpixman\tPixman software renderer\n"
 		"  --shell=NAME\tShell to load, defaults to desktop\n"
@@ -755,6 +758,7 @@ usage(int error_code)
 		"\tflipped-rotate-270\n"
 		"  --use-pixman\t\tUse the pixman (CPU) renderer (deprecated alias for --renderer=pixman)\n"
 		"  --use-gl\t\tUse the GL renderer (deprecated alias for --renderer=gl)\n"
+		"  --use-vulkan\t\tUse the Vulkan renderer (deprecated alias for --renderer=vulkan)\n"
 		"  --no-outputs\t\tDo not create any virtual outputs\n"
 		"  --refresh-rate=RATE\tThe output refresh rate (in mHz)\n"
 		"\n");
@@ -3532,6 +3536,7 @@ load_headless_backend(struct weston_compositor *c,
 	struct wet_backend *wb;
 	bool force_pixman;
 	bool force_gl;
+	bool force_vulkan;
 	bool no_outputs = false;
 	char *transform = NULL;
 
@@ -3544,6 +3549,8 @@ load_headless_backend(struct weston_compositor *c,
 				       false);
 	weston_config_section_get_bool(section, "use-gl", &force_gl,
 				       false);
+	weston_config_section_get_bool(section, "use-vulkan", &force_vulkan,
+				       false);
 	weston_config_section_get_bool(section, "output-decorations", &config.decorate,
 				       false);
 
@@ -3553,6 +3560,7 @@ load_headless_backend(struct weston_compositor *c,
 		{ WESTON_OPTION_INTEGER, "scale", 0, &parsed_options->scale },
 		{ WESTON_OPTION_BOOLEAN, "use-pixman", 0, &force_pixman },
 		{ WESTON_OPTION_BOOLEAN, "use-gl", 0, &force_gl },
+		{ WESTON_OPTION_BOOLEAN, "use-vulkan", 0, &force_vulkan },
 		{ WESTON_OPTION_STRING, "transform", 0, &transform },
 		{ WESTON_OPTION_BOOLEAN, "no-outputs", 0, &no_outputs },
 		{ WESTON_OPTION_INTEGER, "refresh-rate", 0, &config.refresh },
@@ -3562,13 +3570,17 @@ load_headless_backend(struct weston_compositor *c,
 	parse_options(options, ARRAY_LENGTH(options), argc, argv);
 
 	if ((force_pixman && force_gl) ||
-	    (renderer != WESTON_RENDERER_AUTO && (force_pixman || force_gl))) {
+	    (force_pixman && force_vulkan) ||
+	    (force_gl && force_vulkan) ||
+	    (renderer != WESTON_RENDERER_AUTO && (force_pixman || force_gl || force_vulkan))) {
 		weston_log("Conflicting renderer specifications\n");
 		return -1;
 	} else if (force_pixman) {
 		config.renderer = WESTON_RENDERER_PIXMAN;
 	} else if (force_gl) {
 		config.renderer = WESTON_RENDERER_GL;
+	} else if (force_vulkan) {
+		config.renderer = WESTON_RENDERER_VULKAN;
 	} else {
 		config.renderer = renderer;
 	}

include/libweston/libweston.h
@@ -201,7 +201,7 @@ enum weston_hdcp_protection {
  * \ingroup testharness
  */
 struct weston_testsuite_quirks {
-	/** Force GL-renderer to do a full upload of wl_shm buffers. */
+	/** Force GL/Vulkan-renderer to do a full upload of wl_shm buffers. */
 	bool force_full_upload;
 	/** Ensure GL shadow fb is used, and always repaint it fully. */
 	bool gl_force_full_redraw_of_shadow_fb;
@@ -2278,6 +2278,7 @@ enum weston_renderer_type {
 	WESTON_RENDERER_NOOP = 1,
 	WESTON_RENDERER_PIXMAN = 2,
 	WESTON_RENDERER_GL = 3,
+	WESTON_RENDERER_VULKAN = 4,
 };
 
 struct weston_backend *

libweston/backend-drm/drm-gbm.c
@@ -41,6 +41,7 @@
 #include "pixel-formats.h"
 #include "renderer-gl/gl-renderer.h"
 #include "shared/weston-egl-ext.h"
+#include "renderer-vulkan/vulkan-renderer.h"
 #include "linux-dmabuf.h"
 #include "linux-explicit-synchronization.h"
 
@@ -82,6 +83,27 @@ drm_backend_create_gl_renderer(struct drm_backend *b)
 					       &options.base);
 }
 
+static int
+drm_backend_create_vulkan_renderer(struct drm_backend *b)
+{
+	const struct pixel_format_info *format[3] = {
+		b->format,
+		fallback_format_for(b->format),
+	};
+	struct vulkan_renderer_display_options options = {
+		.gbm_device = b->gbm,
+		.formats = format,
+		.formats_count = 1,
+	};
+
+	if (format[1])
+		options.formats_count = 2;
+
+	return weston_compositor_init_renderer(b->compositor,
+					       WESTON_RENDERER_VULKAN,
+					       &options.base);
+}
+
 int
 init_egl(struct drm_backend *b)
 {
@@ -100,7 +122,38 @@ init_egl(struct drm_backend *b)
 	return 0;
 }
 
+int
+init_vulkan(struct drm_backend *b)
+{
+	struct drm_device *device = b->drm;
+
+	b->gbm = gbm_create_device(device->drm.fd);
+	if (!b->gbm)
+		return -1;
+
+	if (drm_backend_create_vulkan_renderer(b) < 0) {
+		gbm_device_destroy(b->gbm);
+		b->gbm = NULL;
+		return -1;
+	}
+
+	return 0;
+}
+
 static void drm_output_fini_cursor_egl(struct drm_output *output)
+{
+	unsigned int i;
+
+	for (i = 0; i < ARRAY_LENGTH(output->gbm_cursor_fb); i++) {
+		/* This cursor does not have a GBM device */
+		if (output->gbm_cursor_fb[i] && !output->gbm_cursor_fb[i]->bo)
+			output->gbm_cursor_fb[i]->type = BUFFER_PIXMAN_DUMB;
+		drm_fb_unref(output->gbm_cursor_fb[i]);
+		output->gbm_cursor_fb[i] = NULL;
+	}
+}
+
+static void drm_output_fini_cursor_vulkan(struct drm_output *output)
 {
 	unsigned int i;
 
@@ -159,6 +212,55 @@ err:
 	weston_log("cursor buffers unavailable, using gl cursors\n");
 	device->cursors_are_broken = true;
 	drm_output_fini_cursor_egl(output);
+	return -1;
+}
+
+static int
+drm_output_init_cursor_vulkan(struct drm_output *output, struct drm_backend *b)
+{
+	struct drm_device *device = output->device;
+	unsigned int i;
+
+	/* No point creating cursors if we don't have a plane for them. */
+	if (!output->cursor_plane)
+		return 0;
+
+	for (i = 0; i < ARRAY_LENGTH(output->gbm_cursor_fb); i++) {
+		struct gbm_bo *bo;
+
+		if (gbm_device_get_fd(b->gbm) != output->device->drm.fd) {
+			output->gbm_cursor_fb[i] =
+				drm_fb_create_dumb(output->device,
+						   device->cursor_width,
+						   device->cursor_height,
+						   DRM_FORMAT_ARGB8888);
+			/* Override buffer type, since we know it is a cursor */
+			output->gbm_cursor_fb[i]->type = BUFFER_CURSOR;
+			output->gbm_cursor_handle[i] =
+				output->gbm_cursor_fb[i]->handles[0];
+		} else {
+			bo = gbm_bo_create(b->gbm, device->cursor_width, device->cursor_height,
+					   GBM_FORMAT_ARGB8888,
+					   GBM_BO_USE_CURSOR | GBM_BO_USE_WRITE);
+			if (!bo)
+				goto err;
+
+			output->gbm_cursor_fb[i] =
+				drm_fb_get_from_bo(bo, device, false, BUFFER_CURSOR);
+			if (!output->gbm_cursor_fb[i]) {
+				gbm_bo_destroy(bo);
+				goto err;
+			}
+			output->gbm_cursor_handle[i] = gbm_bo_get_handle(bo).s32;
+		}
+	}
+
+	return 0;
+
+err:
+	weston_log("cursor buffers unavailable, using vulkan cursors\n");
+	device->cursors_are_broken = true;
+	drm_output_fini_cursor_vulkan(output);
 	return -1;
 }
 
@@ -256,6 +358,123 @@ drm_output_init_egl(struct drm_output *output, struct drm_backend *b)
 	return 0;
 }
 
+static void
+create_gbm_bos(struct gbm_device *gbm, struct drm_output *output, unsigned int n)
+{
+	struct weston_mode *mode = output->base.current_mode;
+	struct drm_plane *plane = output->scanout_plane;
+	struct weston_drm_format *fmt;
+	const uint64_t *modifiers;
+	unsigned int num_modifiers;
+
+	fmt = weston_drm_format_array_find_format(&plane->formats,
+						  output->format->format);
+	if (!fmt) {
+		weston_log("format %s not supported by output %s\n",
+			   output->format->drm_format_name,
+			   output->base.name);
+		return;
+	}
+
+	if (!weston_drm_format_has_modifier(fmt, DRM_FORMAT_MOD_INVALID)) {
+		modifiers = weston_drm_format_get_modifiers(fmt, &num_modifiers);
+		for (unsigned int i = 0; i < n; i++) {
+			output->gbm_bos[i] =
+				gbm_bo_create_with_modifiers(gbm,
+							     mode->width, mode->height,
+							     output->format->format,
+							     modifiers, num_modifiers);
+		}
+	}
+
+	/*
+	 * If we cannot use modifiers to allocate the GBM surface and
+	 * the GBM device differs from the KMS display device, try to
+	 * use linear buffers and hope that the allocated GBM surface
+	 * is correctly displayed on the KMS device.
+	 */
+	if (gbm_device_get_fd(gbm) != output->device->drm.fd)
+		output->gbm_bo_flags |= GBM_BO_USE_LINEAR;
+
+	if (!output->gbm_bos[0]) {
+		for (unsigned int i = 0; i < n; i++) {
+			output->gbm_bos[i] = gbm_bo_create(gbm,
+							   mode->width, mode->height,
+							   output->format->format,
+							   output->gbm_bo_flags);
+		}
+	}
+
+	struct drm_device *device = output->device;
+	for (unsigned int i = 0; i < n; i++) {
+		assert(output->gbm_bos[i]);
+		drm_fb_get_from_bo(output->gbm_bos[i], device, !output->format->opaque_substitute,
+				   BUFFER_GBM_BO);
+	}
+
+	assert(output->gbm_surface == NULL);
+}
+
+/* Init output state that depends on vulkan or gbm */
+int
+drm_output_init_vulkan(struct drm_output *output, struct drm_backend *b)
+{
+	const struct weston_renderer *renderer = b->compositor->renderer;
+	const struct weston_mode *mode = output->base.current_mode;
+	const struct pixel_format_info *format[2] = {
+		output->format,
+		fallback_format_for(output->format),
+	};
+	struct vulkan_renderer_output_options options = {
+		.formats = format,
+		.formats_count = 1,
+		.area.x = 0,
+		.area.y = 0,
+		.area.width = mode->width,
+		.area.height = mode->height,
+		.fb_size.width = mode->width,
+		.fb_size.height = mode->height,
+	};
+
+	assert(output->gbm_surface == NULL);
+
+	/*
+	 * TODO: This method for BO allocation needs to be reworked.
+	 * Currently, it allocates a buffer based on the list of acceptable
+	 * modifiers received from the DRM backend but does not check it
+	 * against formats renderable by the renderer (and there is no
+	 * straightforward way to do so yet).
+	 * Most likely this should be replaced by sending the acceptable
+	 * modifiers list from the DRM backend to the renderer and doing the
+	 * optimal dmabuf allocation in the renderer. But as of this writing,
+	 * this API for dmabuf allocation is not yet implemented in the
+	 * Vulkan renderer.
+	 */
+	create_gbm_bos(b->gbm, output, NUM_GBM_BOS);
+	if (!output->gbm_bos[0]) {
+		weston_log("failed to create gbm bos\n");
+		return -1;
+	}
+	options.num_gbm_bos = NUM_GBM_BOS;
+
+	if (options.formats[1])
+		options.formats_count = 2;
+
+	for (unsigned int i = 0; i < options.num_gbm_bos; i++)
+		options.gbm_bos[i] = output->gbm_bos[i];
+
+	if (renderer->vulkan->output_window_create(&output->base, &options) < 0) {
+		weston_log("failed to create vulkan renderer output state\n");
+		gbm_surface_destroy(output->gbm_surface);
+		output->gbm_surface = NULL;
+		return -1;
+	}
+
+	drm_output_init_cursor_vulkan(output, b);
+
+	return 0;
+}
+
 void
 drm_output_fini_egl(struct drm_output *output)
 {
@@ -274,6 +493,27 @@ drm_output_fini_egl(struct drm_output *output)
 	gbm_surface_destroy(output->gbm_surface);
 	output->gbm_surface = NULL;
 	drm_output_fini_cursor_egl(output);
+}
+
+void
+drm_output_fini_vulkan(struct drm_output *output)
+{
+	struct drm_backend *b = output->backend;
+	const struct weston_renderer *renderer = b->compositor->renderer;
+
+	/* Destroying the GBM surface will destroy all our GBM buffers,
+	 * regardless of refcount. Ensure we destroy them here. */
+	if (!b->compositor->shutting_down &&
+	    output->scanout_plane->state_cur->fb &&
+	    output->scanout_plane->state_cur->fb->type == BUFFER_GBM_BO) {
+		drm_plane_reset_state(output->scanout_plane);
+	}
+
+	renderer->vulkan->output_destroy(&output->base);
+	for (unsigned int i = 0; i < NUM_GBM_BOS; i++)
+		gbm_bo_destroy(output->gbm_bos[i]);
+	output->gbm_surface = NULL;
+	drm_output_fini_cursor_vulkan(output);
 }
 
 struct drm_fb *
@@ -304,6 +544,39 @@ drm_output_render_gl(struct drm_output_state *state, pixman_region32_t *damage)
 		return NULL;
 	}
 	ret->gbm_surface = output->gbm_surface;
+
+	return ret;
+}
+
+struct drm_fb *
+drm_output_render_vulkan(struct drm_output_state *state, pixman_region32_t *damage)
+{
+	struct drm_output *output = state->output;
+	struct drm_device *device = output->device;
+	struct gbm_bo *bo;
+	struct drm_fb *ret;
+
+	output->base.compositor->renderer->repaint_output(&output->base,
+							  damage, NULL);
+
+	bo = output->gbm_bos[output->current_bo];
+	if (!bo) {
+		weston_log("failed to get gbm_bo\n");
+		return NULL;
+	}
+
+	/* Output transparent/opaque image according to the format required by
+	 * the client. */
+	ret = drm_fb_get_from_bo(bo, device, !output->format->opaque_substitute,
+	                         BUFFER_GBM_BO);
+	if (!ret) {
+		weston_log("failed to get drm_fb for bo\n");
+		return NULL;
+	}
+	ret->bo = bo;
+
+	ret->gbm_surface = NULL;
+	output->current_bo = (output->current_bo + 1) % NUM_GBM_BOS;
 
 	return ret;
 }

libweston/backend-drm/drm-internal.h
@@ -293,6 +293,7 @@ enum drm_fb_type {
 	BUFFER_PIXMAN_DUMB, /**< internal Pixman rendering */
 	BUFFER_GBM_SURFACE, /**< internal EGL rendering */
 	BUFFER_CURSOR, /**< internal cursor buffer */
+	BUFFER_GBM_BO, /**< internal Vulkan rendering */
 };
 
 struct drm_fb {
@@ -574,6 +575,8 @@ struct drm_output {
 	int current_cursor;
 
 	struct gbm_surface *gbm_surface;
+	struct gbm_bo *gbm_bos[2];
+	int current_bo;
 	const struct pixel_format_info *format;
 	uint32_t gbm_bo_flags;
 
@@ -946,6 +949,18 @@ drm_output_fini_egl(struct drm_output *output);
 struct drm_fb *
 drm_output_render_gl(struct drm_output_state *state, pixman_region32_t *damage);
 
+int
+init_vulkan(struct drm_backend *b);
+
+int
+drm_output_init_vulkan(struct drm_output *output, struct drm_backend *b);
+
+void
+drm_output_fini_vulkan(struct drm_output *output);
+
+struct drm_fb *
+drm_output_render_vulkan(struct drm_output_state *state, pixman_region32_t *damage);
+
 #else
 inline static int
 init_egl(struct drm_backend *b)
@@ -970,4 +985,29 @@ drm_output_render_gl(struct drm_output_state *state, pixman_region32_t *damage)
 {
 	return NULL;
 }
+
+inline static int
+init_vulkan(struct drm_backend *b)
+{
+	weston_log("Compiled without GBM support\n");
+	return -1;
+}
+
+inline static int
+drm_output_init_vulkan(struct drm_output *output, struct drm_backend *b)
+{
+	return -1;
+}
+
+inline static void
+drm_output_fini_vulkan(struct drm_output *output)
+{
+}
+
+inline static struct drm_fb *
+drm_output_render_vulkan(struct drm_output_state *state, pixman_region32_t *damage)
+{
+	return NULL;
+}
+
 #endif

libweston/backend-drm/drm.c
@@ -520,13 +520,16 @@ drm_output_render(struct drm_output_state *state)
 	    !weston_output_has_renderer_capture_tasks(&output->base) &&
 	    scanout_plane->state_cur->fb &&
 	    (scanout_plane->state_cur->fb->type == BUFFER_GBM_SURFACE ||
+	     scanout_plane->state_cur->fb->type == BUFFER_GBM_BO ||
 	     scanout_plane->state_cur->fb->type == BUFFER_PIXMAN_DUMB)) {
 		fb = drm_fb_ref(scanout_plane->state_cur->fb);
 	} else if (c->renderer->type == WESTON_RENDERER_PIXMAN) {
 		fb = drm_output_render_pixman(state, &damage);
-	} else {
+	} else if (c->renderer->type == WESTON_RENDERER_GL) {
 		fb = drm_output_render_gl(state, &damage);
-	}
+	} else if (c->renderer->type == WESTON_RENDERER_VULKAN) {
+		fb = drm_output_render_vulkan(state, &damage);
+	} else assert(0);
 
 	if (!fb) {
 		drm_plane_state_put_back(scanout_state);
@@ -1192,6 +1195,13 @@ drm_output_apply_mode(struct drm_output *output)
 		drm_output_fini_egl(output);
 		if (drm_output_init_egl(output, b) < 0) {
 			weston_log("failed to init output egl state with "
+				   "new mode");
+			return -1;
+		}
+	} else if (b->compositor->renderer->type == WESTON_RENDERER_VULKAN) {
+		drm_output_fini_vulkan(output);
+		if (drm_output_init_vulkan(output, b) < 0) {
+			weston_log("failed to init output vulkan state with "
 				   "new mode");
 			return -1;
 		}
@@ -2482,6 +2492,11 @@ drm_output_enable(struct weston_output *base)
 			weston_log("Failed to init output pixman state\n");
 			goto err_planes;
 		}
+	} else if (b->compositor->renderer->type == WESTON_RENDERER_VULKAN) {
+		if (drm_output_init_vulkan(output, b) < 0) {
+			weston_log("Failed to init output vulkan state\n");
+			goto err_planes;
+		}
 	} else if (drm_output_init_egl(output, b) < 0) {
 		weston_log("Failed to init output gl state\n");
 		goto err_planes;
@@ -2532,6 +2547,8 @@ drm_output_deinit(struct weston_output *base)
 
 	if (b->compositor->renderer->type == WESTON_RENDERER_PIXMAN)
 		drm_output_fini_pixman(output);
+	else if (b->compositor->renderer->type == WESTON_RENDERER_VULKAN)
+		drm_output_fini_vulkan(output);
 	else
 		drm_output_fini_egl(output);
 
@@ -4218,9 +4235,14 @@ drm_backend_create(struct weston_compositor *compositor,
 	if (config->additional_devices)
 		open_additional_devices(b, config->additional_devices);
 
+	/* GL renderer is the default whenever it is enabled.
+	 * Only on a build without GL but with Vulkan, Vulkan is picked
+	 * as the default. Otherwise, pick pixman as the default */
 	if (config->renderer == WESTON_RENDERER_AUTO) {
-#ifdef BUILD_DRM_GBM
+#if defined(ENABLE_EGL)
 		config->renderer = WESTON_RENDERER_GL;
+#elif defined(ENABLE_VULKAN)
+		config->renderer = WESTON_RENDERER_VULKAN;
 #else
 		config->renderer = WESTON_RENDERER_PIXMAN;
 #endif
@@ -4236,6 +4258,12 @@ drm_backend_create(struct weston_compositor *compositor,
 	case WESTON_RENDERER_GL:
 		if (init_egl(b) < 0) {
 			weston_log("failed to initialize egl\n");
+			goto err_udev_dev;
+		}
+		break;
+	case WESTON_RENDERER_VULKAN:
+		if (init_vulkan(b) < 0) {
+			weston_log("failed to initialize vulkan\n");
 			goto err_udev_dev;
 		}
 		break;

commit ba10e6d81462fc8628dd340ba9404e13745eae18

vulkan-renderer: guard surface output creation with backend defines
Fix compilation errors when compiling with x11 or wayland backends
disabled or not available.

Fixes: 8f56d03d ("libweston: Vulkan renderer")

Signed-off-by: Erico Nunes <nunes.erico@gmail.com>

libweston/renderer-vulkan/vulkan-renderer-internal.h
@@ -35,17 +35,21 @@
 #include <stdbool.h>
 #include <time.h>
 
-#include <wayland-util.h>
 #include <vulkan/vulkan.h>
 #include "shared/helpers.h"
 #include "libweston/libweston.h"
 #include "libweston/libweston-internal.h"
-#include <xcb/xcb.h>
 
-#define VK_USE_PLATFORM_XCB_KHR
+#if defined(BUILD_WAYLAND_COMPOSITOR)
 #define VK_USE_PLATFORM_WAYLAND_KHR
 #include <vulkan/vulkan_wayland.h>
+#endif
+
+#if defined(BUILD_X11_COMPOSITOR)
+#define VK_USE_PLATFORM_XCB_KHR
+#include <xcb/xcb.h>
 #include <vulkan/vulkan_xcb.h>
+#endif
 
 #define MAX_NUM_IMAGES 5
 #define MAX_CONCURRENT_FRAMES 2
@@ -173,10 +177,15 @@ struct vulkan_renderer {
 	struct wl_list pipeline_list;
 	struct dmabuf_allocator *allocator;
 
+#if defined(BUILD_WAYLAND_COMPOSITOR)
 	PFN_vkCreateWaylandSurfaceKHR create_wayland_surface;
-	PFN_vkCreateXcbSurfaceKHR create_xcb_surface;
 	PFN_vkGetPhysicalDeviceWaylandPresentationSupportKHR get_wayland_presentation_support;
+#endif
+
+#if defined(BUILD_X11_COMPOSITOR)
+	PFN_vkCreateXcbSurfaceKHR create_xcb_surface;
 	PFN_vkGetPhysicalDeviceXcbPresentationSupportKHR get_xcb_presentation_support;
+#endif
 
 	PFN_vkGetImageMemoryRequirements2KHR get_image_memory_requirements2;
 	PFN_vkGetMemoryFdPropertiesKHR get_memory_fd_properties;

libweston/renderer-vulkan/vulkan-renderer.c
@@ -303,16 +303,20 @@ static const struct vulkan_extension_table vulkan_inst_ext_table[] = {
 		.name = VK_KHR_SURFACE_EXTENSION_NAME,
 		.flag = EXTENSION_KHR_SURFACE,
 	},
+#if defined(BUILD_WAYLAND_COMPOSITOR)
 	{
 		.name = VK_KHR_WAYLAND_SURFACE_EXTENSION_NAME,
 		.flag = EXTENSION_KHR_WAYLAND_SURFACE,
 		.instance_dep = EXTENSION_KHR_SURFACE,
 	},
+#endif
+#if defined(BUILD_X11_COMPOSITOR)
 	{
 		.name = VK_KHR_XCB_SURFACE_EXTENSION_NAME,
 		.flag = EXTENSION_KHR_XCB_SURFACE,
 		.instance_dep = EXTENSION_KHR_SURFACE,
 	},
+#endif
 };
 
 /* Keep in sync with vulkan-renderer-internal.h. */
@@ -3455,9 +3459,12 @@ vulkan_renderer_output_window_create_swapchain(struct weston_output *output,
 	struct vulkan_renderer *vr = get_renderer(ec);
 	struct vulkan_output_state *vo = get_output_state(output);
 	VkResult result;
-	VkBool32 supported;
+	VkBool32 supported = 0;
 	assert(vulkan_instance_has(vr, EXTENSION_KHR_SURFACE));
 
+	vo->swapchain.surface = VK_NULL_HANDLE;
+
+#if defined(BUILD_WAYLAND_COMPOSITOR)
 	if (options->wayland_display && options->wayland_surface) {
 		assert(vulkan_instance_has(vr, EXTENSION_KHR_WAYLAND_SURFACE));
 
@@ -3472,7 +3479,10 @@ vulkan_renderer_output_window_create_swapchain(struct weston_output *output,
 		result = vr->create_wayland_surface(vr->inst, &wayland_surface_create_info, NULL,
 						    &vo->swapchain.surface);
 		check_vk_success(result, "vkCreateWaylandSurfaceKHR");
-	} else if (options->xcb_connection && options->xcb_window) {
+	}
+#endif
+#if defined(BUILD_X11_COMPOSITOR)
+	if (options->xcb_connection && options->xcb_window) {
 		assert(vulkan_instance_has(vr, EXTENSION_KHR_XCB_SURFACE));
 
 		supported = vr->get_xcb_presentation_support(vr->phys_dev, 0, options->xcb_connection, options->xcb_visualid);
@@ -3486,9 +3496,9 @@ vulkan_renderer_output_window_create_swapchain(struct weston_output *output,
 		result = vr->create_xcb_surface(vr->inst, &xcb_surface_create_info, NULL,
 						&vo->swapchain.surface);
 		check_vk_success(result, "vkCreateXcbSurfaceKHR");
-	} else {
-		assert(0);
 	}
+#endif
+	assert(vo->swapchain.surface != VK_NULL_HANDLE);
 
 	vkGetPhysicalDeviceSurfaceSupportKHR(vr->phys_dev, 0, vo->swapchain.surface, &supported);
 	assert(supported);
@@ -4190,15 +4200,19 @@ load_instance_proc(struct vulkan_renderer *vr, const char *func, void *proc_ptr)
 static void
 vulkan_renderer_setup_instance_extensions(struct vulkan_renderer *vr)
 {
+#if defined(BUILD_WAYLAND_COMPOSITOR)
 	if (vulkan_instance_has(vr, EXTENSION_KHR_WAYLAND_SURFACE)) {
 		load_instance_proc(vr, "vkCreateWaylandSurfaceKHR", &vr->create_wayland_surface);
 		load_instance_proc(vr, "vkGetPhysicalDeviceWaylandPresentationSupportKHR", &vr->get_wayland_presentation_support);
 	}
+#endif
 
+#if defined(BUILD_X11_COMPOSITOR)
 	if (vulkan_instance_has(vr, EXTENSION_KHR_XCB_SURFACE)) {
 		load_instance_proc(vr, "vkCreateXcbSurfaceKHR", &vr->create_xcb_surface);
 		load_instance_proc(vr, "vkGetPhysicalDeviceXcbPresentationSupportKHR", &vr->get_xcb_presentation_support);
 	}
+#endif
 }
 
 static void