2d15bf05677cd54737dc5f238e2cc906ae098bd8
Robert Mader <robert.mader@collabora.com>
2026-04-01 20:47:36 +0200

    gitlab-ci: Bump mesa to 26.0.4
    Notably including fixes for
    1. in-shader blending in llvmpipe
    2. ycbcr support in lavapipe
    
    Signed-off-by: Robert Mader <robert.mader@collabora.com>

--- .gitlab-ci/build-deps.sh
+++ .gitlab-ci/build-deps.sh
@@ -168,9 +168,7 @@ ninja ${NINJAFLAGS} -C build install
 cd ..
 rm -rf glslang
 
-# The 25.3 was the first stable release where the Vulkan backend works with
-# vkms+lavapipe.
-git clone --branch mesa-25.3.2 --depth=1 https://gitlab.freedesktop.org/mesa/mesa.git
+git clone --branch mesa-26.0.4 --depth=1 https://gitlab.freedesktop.org/mesa/mesa.git
 cd mesa
 meson setup build --wrap-mode=nofallback -Dauto_features=disabled \
 	-Dgallium-drivers=llvmpipe -Dvulkan-drivers=swrast -Dvideo-codecs= \
