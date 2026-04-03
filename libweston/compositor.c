qnx/weston

commit df622cbb20f25b4c196c04da1b90fc2e47a005f3

libweston: track clients
This adds the framework to track clients with custom data. The first use
is to associate an ID with each client. The ID is much better suited for
debug printing than a pointer value.

The string representation is stored so that it can be overridden if
desired for compositor forked clients like Xwayland or shell helpers.

Change to a struct in the public header forces a major version bump for
this development cycle.

Signed-off-by: Pekka Paalanen <pekka.paalanen@collabora.com>

libweston/compositor.c
@@ -9895,6 +9895,123 @@ weston_compositor_get_test_data(struct weston_compositor *ec)
 	return ec->test_data.test_private_data;
 }
 
+static void
+weston_client_destroy_handler(struct wl_listener *l, void *data)
+{
+	struct weston_client *client = wl_container_of(l, client,
+						       wl_client_destroy_listener);
+
+	free(client->internal_name);
+	free(client);
+}
+
+static void
+weston_compositor_client_created(struct wl_listener *l, void *data)
+{
+	struct weston_compositor *compositor = wl_container_of(l, compositor,
+							       client_created_listener);
+	struct wl_client *wlclient = data;
+	struct weston_client *client;
+
+	client = xzalloc(sizeof *client);
+	client->wl_client_destroy_listener.notify = weston_client_destroy_handler;
+	wl_client_add_destroy_late_listener(wlclient, &client->wl_client_destroy_listener);
+
+	client->internal_id = ++compositor->client_counter;
+	str_printf(&client->internal_name, "%" PRIu64, client->internal_id);
+}
+
+/** Get weston_client from wl_client
+ *
+ * Every wl_client has an associated weston_client struct which contains client
+ * tracking information.
+ *
+ * \param compositor The compositor instance.
+ * \param wlclient The wl_client to look up, or NULL.
+ * \return If \c wlclient is NULL, returns NULL. Otherwise returns a valid
+ * pointer to the corresponding weston_client.
+ *
+ * \ingroup compositor
+ */
+WL_EXPORT struct weston_client *
+weston_compositor_get_client(struct weston_compositor *compositor,
+			     struct wl_client *wlclient)
+{
+	struct wl_listener *l;
+
+	if (!wlclient)
+		return NULL;
+
+	l = wl_client_get_destroy_late_listener(wlclient, weston_client_destroy_handler);
+	weston_assert_ptr_not_null(compositor, l);
+
+	return container_of(l, struct weston_client, wl_client_destroy_listener);
+}
+
+/** Get internal client ID
+ *
+ * Every client is assigned an ID number from a counter that never resets. The
+ * counter lives in the weston_compositor object. ID 0 is reserved for
+ * "not a client", that is, for the compositor itself.
+ *
+ * \param client The weston_client, or NULL.
+ * \return 0 if \c client is NULL, otherwise the client ID.
+ *
+ * \ingroup client
+ */
+WL_EXPORT uint64_t
+weston_client_get_internal_id(const struct weston_client *client)
+{
+	if (!client)
+		return 0;
+
+	return client->internal_id;
+}
+
+/** Get internal client name
+ *
+ * Every client is assigned a name, usually derived from the internal ID.
+ *
+ * \param client The weston_client, or NULL.
+ * \return "comp" if \c client is NULL, otherwise the client internal name.
+ *
+ * \ingroup client
+ */
+WL_EXPORT const char *
+weston_client_get_internal_name(const struct weston_client *client)
+{
+	if (!client)
+		return "comp";
+
+	return client->internal_name;
+}
+
+/** Set a custom internal name for a client
+ *
+ * This changes what weston_client_get_internal_name() will return.
+ * This will not patch up names already derived from
+ * weston_client_get_internal_name(), so use this immediately after creating
+ * the wl_client.
+ *
+ * \param client The client whose name to set.
+ * \param fmt Standard printf format string and additional arguments.
+ */
+WL_EXPORT void
+weston_client_set_internal_name(struct weston_client *client,
+				const char *fmt, ...)
+{
+	va_list ap;
+	int ret;
+
+	free(client->internal_name);
+	va_start(ap, fmt);
+	ret = vasprintf(&client->internal_name, fmt, ap);
+	va_end(ap);
+
+	if (ret < 0)
+		client->internal_name = NULL;
+}
+
 /** Create the compositor.
  *
  * This functions creates and initializes a compositor instance.
@@ -10051,6 +10168,9 @@ weston_compositor_create(struct wl_display *display,
 	weston_layer_set_position(&ec->fade_layer, WESTON_LAYER_POSITION_FADE);
 	weston_layer_set_position(&ec->cursor_layer,
 				  WESTON_LAYER_POSITION_CURSOR);
+
+	ec->client_created_listener.notify = weston_compositor_client_created;
+	wl_display_add_client_created_listener(ec->wl_display, &ec->client_created_listener);
 
 	ec->debug_scene =
 		weston_compositor_add_log_scope(ec, "scene-graph",
@@ -10522,6 +10642,8 @@ weston_compositor_destroy(struct weston_compositor *compositor)
 	compositor->state = WESTON_COMPOSITOR_OFFSCREEN;
 
 	wl_signal_emit_mutable(&compositor->destroy_signal, compositor);
+
+	wl_list_remove(&compositor->client_created_listener.link);
 
 	weston_compositor_xkb_destroy(compositor);
 

include/libweston/libweston.h
@@ -80,6 +80,7 @@ struct weston_commit_timing_target {
 	struct timespec time;
 };
 
+struct weston_client;
 struct weston_compositor;
 struct weston_surface;
 struct weston_buffer;
@@ -1552,6 +1553,9 @@ struct weston_compositor {
 	 *  surfaces with no output
 	 */
 	struct weston_commit_timing_target requested_repaint_fallback;
+
+	struct wl_listener client_created_listener;
+	uint64_t client_counter;
 };
 
 struct weston_solid_buffer_values {
@@ -2825,6 +2829,21 @@ weston_output_get_supported_color_formats(struct weston_output *output);
 
 void
 weston_output_set_ready(struct weston_output *output);
+
+struct weston_client *
+weston_compositor_get_client(struct weston_compositor *compositor,
+			     struct wl_client *wlclient);
+
+uint64_t
+weston_client_get_internal_id(const struct weston_client *client);
+
+const char *
+weston_client_get_internal_name(const struct weston_client *client);
+
+void
+weston_client_set_internal_name(struct weston_client *client,
+				const char *fmt, ...)
+				__attribute__ ((format (printf, 2, 3)));
 
 #ifdef  __cplusplus
 }

libweston/libweston-internal.h
@@ -250,6 +250,16 @@ struct weston_tearing_control {
 	bool may_tear;
 };
 
+/** A client tracker
+ *
+ * This life time is tied to the wl_client.
+ */
+struct weston_client {
+	struct wl_listener wl_client_destroy_listener;
+	uint64_t internal_id;
+	char *internal_name;
+};
+
 bool
 weston_renderer_resize_output(struct weston_output *output,
 			      const struct weston_size *fb_size,

meson.build
@@ -10,7 +10,7 @@ project('weston',
 	license: 'MIT/Expat',
 )
 
-libweston_major = 15
+libweston_major = 16
 
 # libweston_revision is manufactured to follow the autotools build's
 # library file naming, thanks to libtool

commit f324d3002434b765687e6e96217a0d536197083f

doc: Doxygen group for client
Fix the following error:

Generating doc/sphinx/weston-doc-breathe with a custom command
FAILED: [code=1] doc/sphinx/weston
doc/sphinx/run_doxygen_sphinx.sh
/home/pq/git/weston/libweston/compositor.c:10001: error:
Found non-existing group 'client' for the command '@ingroup',
ignoring command (warning treated as error, aborting now)

Fixes: df622cbb20f25b4c196c04da1b90fc2e47a005f3

Signed-off-by: Pekka Paalanen <pekka.paalanen@collabora.com>

libweston/compositor.c
@@ -100,6 +100,7 @@
  * \defgroup head Head
  * \defgroup output Output
  * \defgroup compositor Compositor
+ * \defgroup client Wayland Clients
  */
 
 #define DEFAULT_REPAINT_WINDOW 7 /* milliseconds */